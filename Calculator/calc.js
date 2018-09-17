5767f97a129926c4b30a77eaa0cc98d5726a9600
syedmeesamali <syedmeesamali@yahoo.com>
2018-09-17 16:36:48 +0400

    Basic functionality and display of numbers added

--- Calculator/calc.js
+++ Calculator/calc.js
@@ -1,25 +1,69 @@
 /*
  * Implement all your JavaScript in this file!
  */
+
+var num1 = '';
+var num2 = '';
+var operator = '';
+var total = '';
+
 $(document).ready(function(){
-   $('#button1').click(function(){
+
         $('button').on('click',function(e){
-            var btn = e.target.innerHTML;
-            if (btn >= 0 && btn <=9) {
-                console.log('number');
+            //Capture the button clicked
+            let btn = e.target.innerHTML;
+            if (btn >= '0' && btn <= '9') {
+            //Separate Handler functions for numbers and Operators
+                handleNumber(btn);
             } else {
-                console.log('operator');
+                handleOperator(btn);
             }
-            
-        })
+   
    });
 
+//Number handling function
 function handleNumber(num) {
-    // code goes here
-}
-function handleOperator(oper) {
-    // code goes here
+    displayBtn(num);
+    var numb1 = num;
 }
 
+//Operator handling function
+function handleOperator(oper) {
+    operator = oper;
+    displayBtn(oper);
+
+}
+
+//Display the current button pressed
+function displayBtn(itemTo) {
+    $('#display').val(itemTo);
+}
+
+function handleTotal() {
+    switch (operator) {
+        case '+':
+            total = +num1 + +num2;
+            displayButton(total);
+            break;
+        case '-':
+            total = +num1 - +num2;
+            displayButton(total);
+            break;
+        case '&#247':
+            total = +num1 / +num2;
+            displayButton(total);
+            break;
+        case '*':
+            total = +num1 * +num2;
+            displayButton(total);
+            break;
+    }
+    updateVariables();
+}
+
+function updateVariables() {
+    num1 = total;
+    num2 = '';
+}
 
 });
