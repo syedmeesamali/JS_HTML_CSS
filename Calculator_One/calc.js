8553bdf102242f236c3e5514dd9a172c10586039
syedmeesamali <syedmeesamali@yahoo.com>
2018-10-09 15:47:15 +0400

    Some final polishing

--- Calculator_One/calc.js
+++ Calculator_One/calc.js
@@ -17,35 +17,19 @@ $(document).ready(function(){
                     if (btn >= '0' && btn <= '9') {
                         handleNumber(btn);
                     } else if (btn == 'C') {
-                        console.log("Num1: "+num1);
-                        console.log("Oper: "+operator);
-                        console.log("Num2: "+num2);
-                        console.log("OpFlag: "+opFlag);
-                        console.log("Num2Flag: "+num2Flag);
                         handleClear();
                     } else if (btn == '=') {
                         if (num2Flag == true) {
                             num2 = $('#display').val();
                             $('#display').val('');
                             num2Flag = false;
-                            console.log("Num1: "+num1);
-                            console.log("Oper: "+operator);
-                            console.log("Num2: "+num2);
-                            console.log("OpFlag: "+opFlag);
-                            console.log("Num2Flag: "+num2Flag);
                             handleTotal();
                         } else {
                             $('#display').val('');
                             handleTotal();
                         }
-                        
                     }
                     else {
-                        console.log("Num1: "+num1);
-                        console.log("Oper: "+operator);
-                        console.log("Num2: "+num2);
-                        console.log("OpFlag: "+opFlag);
-                        console.log("Num2Flag: "+num2Flag);
                         handleOperator(btn);
                     }   
               });
@@ -54,7 +38,6 @@ $(document).ready(function(){
 function handleNumber(num) {    
     if (num1 == '' && opFlag == false) {
             $('#display').val($('#display').val() + num);
-            console.log("first number entry");
         } else if (opFlag == true) {
             $('#display').val('');
             opFlag = false;
@@ -63,7 +46,6 @@ function handleNumber(num) {
         } else {
             num2Flag = true;
             $('#display').val($('#display').val() + num);
-            console.log("second if for opFlag");
         }
         
 }
@@ -107,25 +89,6 @@ function handleTotal() {
         total = (+num1) / (+num2);
         $('#display').val(total);
    }
-    
-    /* switch (operator) {
-        case '+':
-            total = +num1 + +num2;
-            $('#display').val(total);
-            break;
-        case '-':
-            total = +num1 - +num2;
-            $('#display').val(total);
-            break;
-        case '/':
-            total = +num1 / +num2;
-            $('#display').val(total);
-            break;
-        case '*':
-            total = +num1 * +num2;
-            $('#display').val(total);
-            break;
-    } */
     updateVariables();
 }
 
