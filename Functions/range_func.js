ed43bc9a709cfda2155c20baba352c2da5667b27
syedmeesamali <syedmeesamali@yahoo.com>
2018-10-18 16:27:39 +0400

    Sum, range, array functions - IvanOnTech course exercise

--- Functions/range_func.js
+++ Functions/range_func.js
@@ -4,10 +4,18 @@ Returns an array containing all the numbers from start up to and
 (including) end. 
  */
 
-function range(start,end) {
+function range(start,end, step) {
     var myArr = [];
-    for (var k=start; k<=end; k++) {
+    for (var k=start; k<=end; k+=step) {
         myArr.push(k);
     }
     return myArr;
+}
+
+function sumArr(arr) {
+    var total = 0;
+    for (var i in arr) {
+        total += arr[i];
+    }
+    return total;
 }
