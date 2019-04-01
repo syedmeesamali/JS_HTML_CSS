0a936b5da968de4b8a8b3f1937d8e8366db7184a
syedmeesamali <syedmeesamali@yahoo.com>
2019-04-01 16:18:08 +0400

    First order to Gemini exchange using our new BOT

--- 4_BOT-Gemini-NodeJS/index.js
+++ 4_BOT-Gemini-NodeJS/index.js
@@ -1,1 +1,9 @@
-console.log("Welcome to index.js")
+const GeminiAPI = require("gemini-api").default;
+
+const secret = "asdfadsf";
+const key = "mykey";
+
+const restClient = new GeminiAPI({key, secret, sandbox:true});
+
+restClient.newOrder({amount: 10, price: 100, side: "buy", symbol: "btcusd"})
+.then(response => console.log(response));
