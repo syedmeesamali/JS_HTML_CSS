f6db65bcbe023f9bda202a0029f465db47a59e58
syedmeesamali <syedmeesamali@yahoo.com>
2019-04-02 16:53:06 +0400

    Catch block added to handle exceptions. Note that secret and key not required for order book

--- 4_BOT-Gemini-NodeJS/index.js
+++ 4_BOT-Gemini-NodeJS/index.js
@@ -5,8 +5,6 @@ const key = "my_secret_account_Key";
 
 const restClient = new GeminiAPI({key, secret, sandbox:true});
 
-/*
-restClient.newOrder({amount: 10, price: 100, side: "buy", symbol: "btcusd"})
-.then(response => console.log(response)); */
-
-restClient.getOrderBook("btcusd").then(response => console.log(response));
+restClient.newOrder({amount: 10, price: 100, side: "buy", symbol: "btxusd"})
+.then(response => console.log(response))
+.catch(error => console.log(error))
