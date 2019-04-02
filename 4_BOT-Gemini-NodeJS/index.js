fb0621d074d91b2ad4372ce329ff2a66c4e2c288
syedmeesamali <syedmeesamali@yahoo.com>
2019-04-02 16:47:22 +0400

    Get the order book

--- 4_BOT-Gemini-NodeJS/index.js
+++ 4_BOT-Gemini-NodeJS/index.js
@@ -1,9 +1,12 @@
 const GeminiAPI = require("gemini-api").default;
 
-const secret = "asdfadsf";
-const key = "mykey";
+const secret = "my_Secret_Key";
+const key = "my_secret_account_Key";
 
 const restClient = new GeminiAPI({key, secret, sandbox:true});
 
+/*
 restClient.newOrder({amount: 10, price: 100, side: "buy", symbol: "btcusd"})
-.then(response => console.log(response));
+.then(response => console.log(response)); */
+
+restClient.getOrderBook("btcusd").then(response => console.log(response));
