61247090618b3ab3336c4af2fd25994cc01f8d15
syedmeesamali <syedmeesamali@yahoo.com>
2019-04-03 13:12:56 +0400

    Cancelling the orders with API

--- 4_BOT-Gemini-NodeJS/index.js
+++ 4_BOT-Gemini-NodeJS/index.js
@@ -5,6 +5,7 @@ const key = "my_secret_account_Key";
 
 const restClient = new GeminiAPI({key, secret, sandbox:true});
 
-restClient.newOrder({amount: 10, price: 100, side: "buy", symbol: "btxusd"})
+restClient.newOrder({amount:10, price:100, side:"buy", symbol:"btcusd"})
+.then(response => restClient.cancelOrder({order_id: response.order_id}))
 .then(response => console.log(response))
-.catch(error => console.log(error))
+.catch(error => console.log(error));
