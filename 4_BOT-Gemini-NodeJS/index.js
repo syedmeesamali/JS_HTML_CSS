fb99165c6ad7740f5722d2ef915f273f697aefb5
syedmeesamali <syedmeesamali@yahoo.com>
2019-04-07 18:00:07 +0400

    CC pricing with node

--- 4_BOT-Gemini-NodeJS/index.js
+++ 4_BOT-Gemini-NodeJS/index.js
@@ -6,29 +6,13 @@ const key = "my_secret_account_Key";
 const ccAPI_Key = "CryptoCompare_API_Key";
 
 const restClient = new GeminiAPI({key, secret, sandbox:true});
-const cryptoCompare = require("cryptocompare");
-cryptoCompare.setApiKey(ccAPI_Key);
-
-//Below function will provide data of last 169 hours from cryptocompare
-/*
-cryptoCompareAPI.coinList()
-.then(coinList => {
-        console.log(coinList)
-    })
-.catch(console.error)
-*/
-cryptoCompareAPI.priceHistorical('BTC', ['USD', 'EUR']), new Date('2017-01-01')
+const cc = require('cryptocompare')
+cc.setApiKey('ccAPI_Key')
+ 
+// Basic Usage:
+cc.priceHistorical('BTC', ['USD', 'EUR'], new Date('2018-01-01'))
 .then(prices => {
-        console.log(prices)
-    })
+  console.log(prices)
+  // -> { BTC: { USD: 997, EUR: 948.17 } }
+})
 .catch(console.error)
-
-//Below function will provide data of last 169 hours from cryptocompare
-
-/*
-cryptoCompareAPI.histoHour('BTC', 'USD')
-.then(data => {
-        console.log(data)
-    })
-.catch(console.error)
-*/
