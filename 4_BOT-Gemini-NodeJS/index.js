ef8ffb6bf21db6792c9e26cadafc7a4a317e33d6
syedmeesamali <syedmeesamali@yahoo.com>
2019-04-11 21:08:01 +0400

    New working of exchange.js with main index file

--- 4_BOT-Gemini-NodeJS/index.js
+++ 4_BOT-Gemini-NodeJS/index.js
@@ -1,17 +1,8 @@
 global.fetch = require("node-fetch");
-const GeminiAPI = require("gemini-api").default;
 
-const secret = "my_Secret_Key";
-const key = "my_secret_account_Key";
-const ccAPI_Key = "CryptoCompare_API_Key";
-
-const restClient = new GeminiAPI({key, secret, sandbox:true});
-
- 
 const indicators = require("./indicators.js");
+const exchange = require("./market.js");
 
-indicators.HourlymovingAverage('BTC','USD',50, function(result)
-{
-    console.log("MA: ", result)
-})
-
+exchange.marketBuy("btcusd")
+.then(res => console.log(res))
+.catch(console.log(console.error));
