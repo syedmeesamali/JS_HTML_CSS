532ecb3646d30be82c8d1aa0922e617e513f4f6c
syedmeesamali <syedmeesamali@yahoo.com>
2019-04-26 17:51:22 +0400

    Updates to the moving average functions

--- 4_BOT-Gemini-NodeJS/indicators.js
+++ 4_BOT-Gemini-NodeJS/indicators.js
@@ -2,8 +2,13 @@ const cc = require('cryptocompare')
 cc.setApiKey('ccAPI_Key')
 
 module.exports = 
+const ccAPI_Key = "41067c4a78f2232322cca0494c3d1f7ea96414c32a75d19ca483f8e0710de13d";
+const cc = require('cryptocompare')
+cc.setApiKey('ccAPI_Key')
+
+module.exports = 
 {
-// 100-hour moving average function
+// Hourly moving average function
 HourlymovingAverage:function(cryptoAsset, Fiat, Hours, callback)
 {
     if (Hours > 169 )
@@ -22,9 +27,34 @@ HourlymovingAverage:function(cryptoAsset, Fiat, Hours, callback)
             sum += data[i].close;
         }
         var movingAvg = sum / Hours;
-        callback(movingAvg);
+        callback(HourlymovingAverage);
     })
     .catch(console.error)
-}
+} //End of the function for hourly averages
+
+// Daily
+DailymovingAverage:function(cryptoAsset, Fiat, Days, callback)
+{
+    if (Days > 200 )
+    {
+        console.error("Only 200 hours allowed !")
+        return
+    }
+
+    cc.histoDay(cryptoAsset, Fiat)
+    .then(data => {
+        data = data.reverse()
+        var sum = 0;
+    
+        for (i=0; i<Days; i++) 
+        {
+            sum += data[i].close;
+        }
+        var movingAvg = sum / Days;
+        callback(DailymovingAverage);
+    })
+    .catch(console.error)
+} //End of the function for hourly averages
+
 
 }//End of module exports
