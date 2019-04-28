446bbb851170ad2ddcfb49a1da0f55ce55f520f5
syedmeesamali <syedmeesamali@yahoo.com>
2019-04-28 17:46:24 +0400

    Building up a strategy based on MA

--- 4_BOT-Gemini-NodeJS/index.js
+++ 4_BOT-Gemini-NodeJS/index.js
@@ -4,8 +4,23 @@ const exchange = require("./market.js");
 
 var strategy = function()
 {
+    console.log("            ");
+    console.log("===================");
     console.log("Executing strategy!");
-    setTimeout(strategy, 1000);
+    
+    indicators.HourlymovingAverage("BTC", "USD", 100, function(ma)
+    {
+        exchange.bitcoinPrice()
+        .then(res => 
+        {
+            var price = res.last;
+            console.log("MA: ", ma);
+            console.log("Price: ", price);
+            setTimeout(strategy, 1000);
+        })
+    });
+    
+
 }
 
 strategy()
