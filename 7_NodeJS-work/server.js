6ab2cd636575e99a67e709268facc933340ffb00
syedmeesamali <syedmeesamali@yahoo.com>
2019-05-24 23:58:44 +0400

    alternate function

--- 7_NodeJS-work/server.js
+++ 7_NodeJS-work/server.js
@@ -1,24 +1,13 @@
 var http = require('http');
 var fs = require('fs');
 
-function onRequest(request, response)
-{
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    //response.write('Hello World');
-    //response.end();
-    
-    fs.readFile('/hello.txt', (error, data) =>
-    {
-        if(error)
-        {
-            response.writeHead(404);
-            response.write('File Not Found');
-        } else
-        {
-            response.write(data);
-        }
-        response.end;
-    }); 
-}
+var server = http.createServer(function(req, res){
+    console.log("Request was: " + req.url);
+    res.writeHead(200, {'content-type': 'text/plain'});
+    var readStream = fs.createReadStream(_dirname + '/hello.txt', 'utf8');
+    readStream.pipe(res);
 
-http.createServer(onRequest).listen(8000);
+});
+
+server.listen(3000,'127.0.0.1');
+console.log("Server working now");
