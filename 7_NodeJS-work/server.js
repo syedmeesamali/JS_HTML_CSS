28507f8dc4fdbc8d453d36755dc0c16a60fcb195
syedmeesamali <syedmeesamali@yahoo.com>
2019-05-24 06:05:24 +0400

    N

--- 7_NodeJS-work/server.js
+++ 7_NodeJS-work/server.js
@@ -1,8 +1,24 @@
-var http = require('http')
-var fs = require('fs')
+var http = require('http');
+var fs = require('fs');
 
 function onRequest(request, response)
 {
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    fs.readFile
+    response.write('Hello World');
+    response.end();
+    /*
+    fs.readFile('./index.html', null, function(error, data) 
+    {
+        if(error)
+        {
+            response.writeHead(404);
+            response.write('File Not Found');
+        } else
+        {
+            response.write(data);
+        }
+    })
+    */
 }
+
+http.createServer(onRequest).listen(8000);
