033f31b36f51f0f0cdbaa82b44992c51564e8a73
syedmeesamali <syedmeesamali@yahoo.com>
2019-05-27 14:56:18 +0400

    Response and redirection in NodeJs

--- 7_NodeJS-work/server.js
+++ 7_NodeJS-work/server.js
@@ -2,11 +2,12 @@ var http = require('http');
 var fs = require('fs');
 
 var server = http.createServer(function(req, res){
-    console.log("Request was: " + req.url);
-    res.writeHead(200, {'content-type': 'text/html'});
-    var readStream = fs.createReadStream('./index.html', 'utf8');
-    readStream.pipe(res);
-
+    console.log("Request was made: " + req.url);
+    if (req.url === '/Home' || req.url === '/')
+    {
+        res.writeHead(200, {'content-type': 'text/html'});
+        fs.createReadStream('./index.html').pipe(res);
+    }
 });
 
 server.listen(3000,'127.0.0.1');
