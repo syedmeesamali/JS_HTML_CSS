d8bd04733c8c3f86d0b526187f9476dcf85f2542
syedmeesamali <syedmeesamali@yahoo.com>
2019-05-29 22:22:32 +0400

    Routing parameters nodejs

--- 7_NodeJS-work/app2.js
+++ 7_NodeJS-work/app2.js
@@ -11,4 +11,11 @@ app.get('/contact', function(req, res)
     res.send('this is a contact page');
 });
 
+//Special routing parameters below
+
+app.get('/profile/:id', function(req, res)
+{
+    res.send('Your requested to see ID of ' + req.params.id);
+});
+
 app.listen(3000);
