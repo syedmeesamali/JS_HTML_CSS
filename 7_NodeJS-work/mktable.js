76db06d24fe845f44bf2aeead69ced7f10403769
syedmeesamali <syedmeesamali@yahoo.com>
2019-07-13 19:07:21 +0400

    Table created using nodejs

--- 7_NodeJS-work/mktable.js
+++ 7_NodeJS-work/mktable.js
@@ -3,15 +3,18 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
     host: "localhost",
     user: "meesam",
-    password: "admin"
+    password: "admin",
+    database: "mydb"
   });
 
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected");
+  var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+
   con.query(sql, function (err, result) {
     if (err) throw err;
-    console.log("Result is: " + result);
+    console.log("Table Created");
   })
 })
 
