dcb9ccac8b77167ab4c96ada0bdb101ac394f860
syedmeesamali <syedmeesamali@yahoo.com>
2019-07-14 02:33:26 +0400

    alteration of existing table using nodejs

--- 7_NodeJS-work/mktable.js
+++ 7_NodeJS-work/mktable.js
@@ -10,11 +10,11 @@ var con = mysql.createConnection({
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected");
-  var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+  var sql = "ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY";
 
   con.query(sql, function (err, result) {
     if (err) throw err;
-    console.log("Table Created");
+    console.log("Table Altered successfully.");
   })
 })
 
