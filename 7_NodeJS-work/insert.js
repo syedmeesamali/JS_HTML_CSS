f70f7fd8a59c4db580a28d58dd4f27a131a60fcb
syedmeesamali <syedmeesamali@yahoo.com>
2019-07-14 16:39:36 +0400

    Inserting record into existing table of database

---
+++ 7_NodeJS-work/insert.js
@@ -0,0 +1,20 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "meesam",
+    password: "admin",
+    database: "mydb"
+  });
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected");
+  var sql = "INSERT INTO customers (name, address) VALUES ('CCL Company', 'Detroit Street')";
+
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted ..");
+  })
+})
+
