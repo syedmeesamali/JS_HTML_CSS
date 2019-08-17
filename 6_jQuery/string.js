59d83d6ea2acf0f8641a7418388b663374062008
syedmeesamali <syedmeesamali@yahoo.com>
2019-08-17 21:48:27 +0400

    JS teasing

---
+++ 6_jQuery/string.js
@@ -0,0 +1,10 @@
+var username;
+var message; 
+username = "Meesam Ali";
+message = "See our upcoming Range!";
+
+var elName = document.getElementById('name');
+elName.textContent = username;
+
+var elNote = document.getElementById('note');
+elNote.textContent = message;
