9d8a488260a1bae399a02378d388f2e8119ba2d6
syedmeesamali <syedmeesamali@yahoo.com>
2019-08-21 18:05:07 +0400

    some websocket work

--- 6_jQuery/server.js
+++ 6_jQuery/server.js
@@ -4,12 +4,19 @@ const websocket = require('ws');
 
 const server = http.createServer((req, res) => {
     res.end("I am connected now");
-})
+});
 
-const wss = new websocket.server({ server });
+const wss = new websocket.Server({ server });
 wss.on('headers', (headers, req) => {
     console.log(headers);
-})
+});
 
+//Event connection
+wss.on('connection', (ws, req) => {
+    ws.send('This is message from server');
+    ws.on('message', (msg) => {
+        console.log(msg);
+    });
+});
 //Listen at port 8000
 server.listen(8000);
