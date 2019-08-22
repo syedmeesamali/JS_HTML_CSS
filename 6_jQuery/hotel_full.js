b6962c38dc60712b4f09f0802a1b5ad101aaa6f6
syedmeesamali <syedmeesamali@yahoo.com>
2019-08-22 23:51:09 +0400

    completion of hotel related code

--- 6_jQuery/hotel_full.js
+++ 6_jQuery/hotel_full.js
@@ -26,7 +26,17 @@
         var weekFromToday, day, date, month, year, dayNames, monthNames;
         weekFromToday = new Date(today.getTime() + 7*24*60*60*1000);
         dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-        
+        monthNames = ["January","February","March","April","May","June","July","August","September",
+        "October","November","December"];
+        day = dayNames[weekFromToday.getDay()];
+        date = weekFromToday.getDate();
+        month = monthNames[weekFromToday.getMonth()];
+        year = weekFromToday.getFullYear();
+
+        expiryMsg = 'Offer expires next ';
+        expiryMsg += day + ' <br /> ('  + date + ' ' + month + ' ' + year + ')';
+        return expiryMsg;
+
     }
 
 
