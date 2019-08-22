9c143a4340ba233c524fd81c1727772fe88d3ae7
syedmeesamali <syedmeesamali@yahoo.com>
2019-08-22 23:46:53 +0400

    hotel file updates

--- 6_jQuery/hotel_full.js
+++ 6_jQuery/hotel_full.js
@@ -18,6 +18,16 @@
     roomRate.textContent = '$' + hotel.roomRate.toFixed(2);
     specialRate.textContent = '$' + hotel.offerPrice();
 
+    var expiryMsg;
+    var today;
+    var elEnds;
+
+    function offerExpires(today) {
+        var weekFromToday, day, date, month, year, dayNames, monthNames;
+        weekFromToday = new Date(today.getTime() + 7*24*60*60*1000);
+        dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        
+    }
 
 
 }()); //End of immediately invoked function expression IIFE
