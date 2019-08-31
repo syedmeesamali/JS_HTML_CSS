18eb742658fd1312e9c066b1607efa47b7b916c1
syedmeesamali <syedmeesamali@yahoo.com>
2019-08-31 23:54:17 +0400

    LocalStorage function implementation

--- 8_Projects/1_Twitter_List/js/app.js
+++ 8_Projects/1_Twitter_List/js/app.js
@@ -26,7 +26,8 @@ function newTweet(e) {
     //Add remove button to each tweet
     li.appendChild(removeBtn);
     tweetList.appendChild(li);
-
+    //Add the tweet to local storage
+    tweetsToStorage(tweet);
 }
 
 //Function to remove a tweet
@@ -34,4 +35,18 @@ function removeTweet(e) {
     if(e.target.classList.contains('remove-tweet')) {
         e.target.parentElement.remove();
     } 
+}
+
+//Add tweets to the local storage
+function tweetsToStorage(e) {
+    console.log("Hello from local storage function");
+}
+
+//This function will retrieve tweets from localstorage
+function getTweetsFromStorage() {
+    let tweets;
+    //Get the values, if null is returned then create empty array
+    if (localStorage.getItem('tweets') == null) {
+        tweets = [];
+    }
 }
