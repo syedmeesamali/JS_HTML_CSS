088639727727dffa5d4793a7a90b5daae3386cfe
syedmeesamali <syedmeesamali@yahoo.com>
2019-09-03 12:46:16 +0400

    onblur and onfocus events and functions

--- 6_jQuery/JSevents.js
+++ 6_jQuery/JSevents.js
@@ -1,3 +1,13 @@
+function blurFunction(){
+    var fd = document.getElementById('len');
+    fd.className = 'changeNow';
+  }
+
+  function focusFunction(){
+    var fd = document.getElementById('len');
+    fd.className = 'Normal';
+  }
+
 function checkUsername(e, minLength) {
     var target = e.target;
 }
@@ -16,7 +26,7 @@ function checkLength(e, minLength) {
       }
   }
 
-  var elUserName = document.getElementById('user-name');
+  var elUserName = document.getElementById('len');
   if (elUserName.addEventListener) {
       elUserName.addEventListener('blur', function(e) {
           checkUsername(e, 5);
@@ -26,3 +36,5 @@ function checkLength(e, minLength) {
           checkUsername(e, 5);
       });
   }
+
+
