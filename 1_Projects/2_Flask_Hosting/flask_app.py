5b9b570a93623829734369465505e945ae2c9c44
syedmeesamali <syedmeesamali@yahoo.com>
2019-09-05 12:20:46 +0400

    Static folder with flask

--- 1_Projects/2_Flask_Hosting/flask_app.py
+++ 1_Projects/2_Flask_Hosting/flask_app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 app = Flask(__name__)
 @app.route('/')
 def index():
@@ -6,8 +7,14 @@ def index():
 
 @app.route('/material')
 def mat():
-    return render_template("mat_calc.html")
+    file1 = os.path.join('static','main1.png')
+    file2 = os.path.join('static','main2.gif')
+    return render_template("mat_calc.html", image1 = file1, image2 = file2)
 
 @app.route('/bird')
 def bird():
     return render_template("bird.html")
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host = 'localhost',port=8000)
