d8dce6a49fcc07c8f86af5488883d80d3124ccb9
syedmeesamali <syedmeesamali@yahoo.com>
2019-09-09 23:43:02 +0400

    Draw main file

---
+++ 5_JavaScript/draw2.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  //State variable
+  let draw = false;
+
+  //Elements
+  let points = [];
+  let lines = [];
+  let svg = null;
+
+  function render() {
+      svg = d3.select('#draw')
+        .attr('height', window.innerHeight)
+        .attr('width', window.innerWidth)
+
+      svg.on('mousedown', function() {
+          draw = true;
+          const coords = d3.mouse(this);
+          draw_point(coords[0], coords[1], false);
+      });
+    
+      svg.on('mouseup', () => {
+        draw = false;
+      });
+    
+      svg.on('mousemove', draw_point);
+  }
+
+  function draw_point() {
+      if (draw === false) 
+      {
+        return ;
+      }
+    const coords = d3.mouse(this);
+    draw_point(coords[0], coords[1], true);
+  };
+  
+  document.querySelector('#erase').onclick = () => {
+      for (let i=0; i<points.length; i++) 
+        points[i].remove();
+      for (let j=0; j<lines.length; j++)
+        lines[i].remove();
+    
+        points = [];
+        lines = [];
+  }
+
+
+}); //End of main function
