80b0188e758adfef67ec40a8dc0aca419f286a24
syedmeesamali <syedmeesamali@yahoo.com>
2019-09-16 15:53:43 +0400

    Updated bash files - lot of study and work today for BASH

--- 8_BASH/bash1.sh
+++ 8_BASH/bash1.sh
@@ -1,6 +1,3 @@
-# Hello World Program in Bash Shell
-
-
 read input
 
 proto="$(echo $input | grep :// | sed -e's,^\(.*://\).*,\1,g')"
@@ -11,11 +8,8 @@ user="$(echo $url | grep @ | cut -d@ -f1)"
 hostport="$(echo ${url/$user@/} | cut -d/ -f1)"
 host="$(echo $hostport | sed -e 's,:.*,,g')"
 IFS='.'
-
-#Extract the path if any
 path="$(echo $url | grep / | cut -d/ -f2-)"
 domain="$(echo "$url" | sed -e 's|^/[^/]*//||' -e 's|/.*$||')"
-
 echo "url: $url"
 echo "   Protocol: $proto"
 echo "   User: $user"
