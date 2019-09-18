33d7b45d95708684899531b4eb18eb35ecc7c9c0
syedmeesamali <syedmeesamali@yahoo.com>
2019-09-18 16:42:40 +0400

    some bash updates

--- 8_BASH/bash1.sh
+++ 8_BASH/bash1.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 read input
-
 proto="$(echo $input | grep :// | sed -e's,^\(.*://\).*,\1,g')"
-
-
 url="$(echo ${input/$proto/})"
-user="$(echo $url | grep @ | cut -d@ -f1)"
 hostport="$(echo ${url/$user@/} | cut -d/ -f1)"
-host="$(echo $hostport | sed -e 's,:.*,,g')"
-IFS='.'
-path="$(echo $url | grep / | cut -d/ -f2-)"
+host="$(echo $hostport | cut -d. -f1)"
+path="$(echo $url | cut -d. -f2-)"
 domain="$(echo "$url" | sed -e 's|^/[^/]*//||' -e 's|/.*$||')"
 echo "url: $url"
 echo "   Protocol: $proto"
-echo "   User: $user"
 echo "   Host: $host"
 echo "   path: $path"
 echo "   domain: $domain"
