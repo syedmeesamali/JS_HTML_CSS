e427f3fd95d8002bb0a21ca2d7ff06f14156cb7b
syedmeesamali <syedmeesamali@yahoo.com>
2019-09-20 23:34:38 +0400

    Updates to the infinite scroll example

--- 1_Projects/3_Multi-page/flask_app.py
+++ 1_Projects/3_Multi-page/flask_app.py
@@ -13,7 +13,14 @@ def posts():
     start = int(request.form.get("start") or 0)
     end = int(request.form.get("end") or (start + 9))
 
+    #Generate list of posts
+    data = []
+    for i in range(start, end+1):
+        data.append(f"Post # {i}")
 
+    #Artificially delay speed of mouse
+    time.sleep(1)
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run()
