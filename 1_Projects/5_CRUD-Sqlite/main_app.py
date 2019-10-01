ac8df9dfadfbb7535615a29f0f53e2b997d6b136
syedmeesamali <syedmeesamali@yahoo.com>
2019-10-01 15:42:31 +0400

    crud app with sqlite3

---
+++ 1_Projects/5_CRUD-Sqlite/main_app.py
@@ -0,0 +1,13 @@
+from flask import *
+import sqlite3
+
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+
+if __name__ == "__main__":
+    app.run()
