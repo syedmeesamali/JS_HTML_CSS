1d31295a82ed0fefd998976798f491f84cac45e3
syedmeesamali <syedmeesamali@yahoo.com>
2019-11-12 12:29:37 +0400

    lot of useful updates and ID system for all databases

--- 1_Projects/2_Flask_Hosting/flask_app.py
+++ 1_Projects/2_Flask_Hosting/flask_app.py
@@ -4,7 +4,7 @@ import os
 from datetime import datetime
 
 database = "./database.db"
-link_db = "./linkdb.db"
+link_db = "./links.db"
 read_db = "./read.db"
 db_ideas = "./ideas.db"
 db_done = "./done.db"
@@ -84,7 +84,7 @@ def ideasdone():
     return render_template("ideasdone.html", links = res)
 
 @app.route('/done/<int:val>', methods = ["POST", "GET"])
-def read(val):
+def done(val):
     if request.method == "POST":
         msg = "msg"
         try:
@@ -116,6 +116,39 @@ def read(val):
             conn2.close()
     return render_template("index.html")
 
+@app.route('/read/<int:val>', methods = ["POST", "GET"])
+def read(val):
+    if request.method == "POST":
+        msg = "msg"
+        try:
+            conn = sqlite3.connect(link_db)
+            conn2 = sqlite3.connect(read_db)
+            cur = conn.cursor()
+            cur1 = conn2.cursor()
+            cur.execute("SELECT ID FROM links WHERE ID = ?", (val,))
+            id = cur.fetchone()[0]
+            cur.execute("SELECT Desc FROM links WHERE ID = ?", (val,))
+            Desc = cur.fetchone()[0]
+            cur.execute("SELECT linkUrl FROM links WHERE ID = ?", (val,))
+            linkUrl = cur.fetchone()[0]
+            cur.execute("SELECT linkType FROM links WHERE ID = ?", (val,))
+            linkType = cur.fetchone()[0]
+            cur.execute("SELECT added FROM links WHERE ID = ?", (val,))
+            Date = cur.fetchone()[0]
+            timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            cur1.execute("INSERT INTO read (ID, desc, url, type, added, read) VALUES(?, ?, ?, ?, ?, ?)" , (id, Desc, linkUrl, linkType, Date, timestamp))
+            cur.execute("DELETE FROM links WHERE ID = ?", (val,))
+            conn2.commit()
+            conn.commit()
+            msg = "Data Entered Successfully!"
+        except:
+            conn.rollback()
+            conn2.rollback()
+            msg = "Couldn't update the databases.....!"
+        finally:
+            return render_template("success.html", msg = msg)
+            conn.close()
+
 @app.route('/entry')
 def entry():
     conn = sqlite3.connect(database)
@@ -125,7 +158,10 @@ def entry():
 
 @app.route('/links')
 def links():
-    return render_template("code5.html")
+    conn = sqlite3.connect(link_db)
+    cur = conn.cursor()
+    res = cur.execute("SELECT * FROM links")
+    return render_template("links.html", links = res)
 
 @app.route('/ideaentry')
 def ideaentry():
@@ -135,7 +171,7 @@ def ideaentry():
 def readlinks():
     conn = sqlite3.connect(read_db)
     cur = conn.cursor()
-    res = cur.execute("SELECT * FROM readlinks")
+    res = cur.execute("SELECT * FROM read")
     return render_template("readlinks.html", links = res)
 
 @app.route('/')
