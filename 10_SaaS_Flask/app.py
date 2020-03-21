35c85599e54fb7080fbe25f66cc00e701761f6cf
syedmeesamali <syedmeesamali@yahoo.com>
2020-03-21 21:42:45 +0400

    Continuation on flask app work

--- 10_SaaS_Flask/app.py
+++ 10_SaaS_Flask/app.py
@@ -5,7 +5,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     user = {'username': 'ali shah'}
-    return render_template('index.html', user = user)
+    posts = [{
+            'author': {'username': 'Meesam'},
+            'body': 'Beautiful rainy day in Dubai'
+        }, {
+            'author': {'username': 'Rabbit'},
+            'body': 'Beautiful life is the meaningful life'
+        }]
+    return render_template('index.html', user = user, posts = posts)
 
 if __name__ == "__main__":
     app.run(debug=True)
