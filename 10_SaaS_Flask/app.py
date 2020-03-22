aa48764a019f4bd388e497cf6213751adedcfad0
syedmeesamali <syedmeesamali@yahoo.com>
2020-03-22 23:45:47 +0400

    Interesting python and flask examples

--- 10_SaaS_Flask/app.py
+++ 10_SaaS_Flask/app.py
@@ -14,5 +14,15 @@ def index():
         }]
     return render_template('index.html', user = user, posts = posts)
 
+@app.route('/puppy_latin/<name>')
+def puppylatin(name):
+    pupname = ''
+    if name[-1] == 'y':
+        pupname = name[:-1] + 'iful'
+    else:
+        pupname = name + 'y'
+    return "<h2> Your puppy latin name is: {}".format(pupname)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
