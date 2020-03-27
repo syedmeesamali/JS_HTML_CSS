05c0ccf0b58e9771ae4da1c6a34ba9c75596a809
syedmeesamali <syedmeesamali@yahoo.com>
2020-03-27 23:45:45 +0400

    Using templates and blocks

--- 1_Projects/7_FlaskNew/app/routes.py
+++ 1_Projects/7_FlaskNew/app/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello flask again'
+    user = {'username': 'Syed Meesam Ali'}
+    return render_template('index.html', title='Home', user = user)
