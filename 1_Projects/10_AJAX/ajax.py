e4239250461f1ade5b3e21d78ad7678314812427
syedmeesamali <syedmeesamali@yahoo.com>
2020-05-14 12:43:52 +0400

    Simple ajax issues
    Signed-off-by: syedmeesamali <syedmeesamali@yahoo.com>

--- 1_Projects/10_AJAX/ajax.py
+++ 1_Projects/10_AJAX/ajax.py
@@ -1,37 +1,17 @@
 from flask import *
 import time
 
-
-form_data = "./form_data.db"
 app = Flask(__name__)
 
-@app.route("/", methods = ["POST", "GET"])
+@app.route("/")
 def main():
-    return render_template("index.html", reload = time.time())
+    return render_template("index.html")
 
-@app.route("/api/info")
-def api_info():
-    info = {
-        "ip": "127.0.0.1",
-        "hostname" : "ali",
-        "desc" : "server",
-        "load" : [3.21, 4.75, 14]
-    }
-    return jsonify(info)
-
-@app.route("/api/calc")
-def add():
+@app.route("/_addNumbers")
+def addNumbers():
     a = int(request.args.get('a', 0))
     b = int(request.args.get('b', 0))
-    div = 'na'
-    if b != 0:
-        div = a/b
-    return jsonify({
-        "a" : a,
-        "b" : a,
-        "add" : a + b,
-        "multiply" : a * b,
-    })
+    return jsonify(result = a + b)
 
 if __name__ == "__main__":
     app.run(debug=True) 
