8fdf7384860bab392df7ad589916f1f81e7c335a
syedmeesamali <syedmeesamali@yahoo.com>
2020-05-17 16:19:44 +0400

    Some more ajax tinkering
    Signed-off-by: syedmeesamali <syedmeesamali@yahoo.com>

---
+++ 1_Projects/10_AJAX/Ex_2/ajax.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, jsonify, make_response
+import time
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template("index.html")
+
+@app.route('/process', methods=['POST'])
+def process():
+    req = request.get_json()
+    print(req)
+    res = make_response(jsonify({"message": "OK"}),200)
+    return res
+
+if __name__ == "__main__":
+    app.run(debug=True) 
