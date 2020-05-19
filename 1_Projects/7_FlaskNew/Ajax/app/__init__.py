df94b434315c3ab35b6658fdf0049846d3325e5b
syedmeesamali <syedmeesamali@yahoo.com>
2020-05-19 23:39:52 +0400

    Simple structure working perfectly. need to extend and run my full website on this base structure.

--- 1_Projects/7_FlaskNew/Ajax/app/__init__.py
+++ 1_Projects/7_FlaskNew/Ajax/app/__init__.py
@@ -1,12 +1,25 @@
-import os
+#import os
 from flask import Flask
 #from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-app=Flask(__name__)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+#from config import app_config
 
-from app import views, models
-#app.config.from_object(Config)
+app=Flask(__name__, instance_relative_config=True)
+# db = SQLAlchemy(app)
+# migrate = Migrate(app, db)
+
+from app import views
+app.config.from_object('config')
+
+# def create_app(config_name):
+#     app = Flask(__name__, instance_relative_config=True)
+#     app.config.from_object(app_config[config_name])
+#     app.config.from_pyfile('config.py')
+#     db.init_app(app)
+#     @app.route('/')
+#     def hello_world():
+#         return 'Helllo world'
+
+#     return app
--- 1_Projects/7_FlaskNew/Ajax/config.py
+++ 1_Projects/7_FlaskNew/Ajax/config.py
@@ -4,8 +4,20 @@
 DEBUG = True
 
 # class Config(object):
-#     SECRET_KEY = os.environ.get('SECRET_KEY') or 'You-will-never-guess'
+#     SECRET_KEY = 'lkjalsdjfkasjdfewwer234234'
 #     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
 #     'sqlite:///' + os.path.join(basedir, 'app.db')
 #     SQLALCHEMY_TRACK_NOTIFICATIONS = False
 
+
+# class DevelopmentConfig(Config):
+#     DEBUG = True
+#     SQLALCHEMY_ECHO = True
+
+# class ProductionConfig(Config):
+#     DEBUG = False
+    
+# app_config = {
+#     'development': DevelopmentConfig,
+#     'production': ProductionConfig
+# }
