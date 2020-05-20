32f8bf6c4f09b53f75934e19b6c792bde12fcb76
syedmeesamali <syedmeesamali@yahoo.com>
2020-05-20 15:43:08 +0400

    Updates to the new testing site - need to check with fetch and db updates - may use MYSQL
    Signed-off-by: syedmeesamali <syedmeesamali@yahoo.com>

--- 1_Projects/7_FlaskNew/Ajax/app/__init__.py
+++ 1_Projects/7_FlaskNew/Ajax/app/__init__.py
@@ -1,25 +1,8 @@
-#import os
 from flask import Flask
-#from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-#from config import app_config
-
 app=Flask(__name__, instance_relative_config=True)
-# db = SQLAlchemy(app)
-# migrate = Migrate(app, db)
 
 from app import views
 app.config.from_object('config')
-
-# def create_app(config_name):
-#     app = Flask(__name__, instance_relative_config=True)
-#     app.config.from_object(app_config[config_name])
-#     app.config.from_pyfile('config.py')
-#     db.init_app(app)
-#     @app.route('/')
-#     def hello_world():
-#         return 'Helllo world'
-
-#     return app
--- 1_Projects/7_FlaskNew/Ajax/app/views.py
+++ 1_Projects/7_FlaskNew/Ajax/app/views.py
@@ -1,7 +1,29 @@
-from flask import render_template
-
+from flask import render_template, request, jsonify
 from app import app
+from datetime import datetime
+
+form_data = "./form_data.db"
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
+@app.route('/save_form', methods=['POST'])
+def save_form():
+    #msg = "msg"
+    try:
+        Name = request.form["name"]
+        Email = request.form["email"]
+        Title = request.form["title"]
+        Message = request.form["message"]
+        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        conn = sqlite3.connect(form_data)
+        cur = conn.cursor()
+        cur.execute("INSERT INTO form_data (Name, Email, Title, Message, Date) VALUES(?, ?, ?, ?, ?)" , (Name, Email, Title, Message, timestamp))
+        conn.commit()
+        return jsonify({'Message': 'Thanks for your message!'})
+    except:
+        conn.rollback()
+        return jsonify({'error': 'Sorry your message couldn\'t be delivered!'})
+    conn.close()
