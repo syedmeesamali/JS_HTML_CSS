318c9d273de0441b61c7c151dd24f4bee48c2388
syedmeesamali <syedmeesamali@yahoo.com>
2020-05-23 19:15:26 +0400

    Tetris items arrays

--- 1_Projects/11_JS_Tetris/tetris.js
+++ 1_Projects/11_JS_Tetris/tetris.js
@@ -5,5 +5,38 @@ document.addEventListener('DOMContentLoaded', () => {
     const scoreDisp = document.querySelector('#score');
     const startButton = document.querySelector('#start-button');
     
-    console.log(squares);
+    const L_tetris = [
+    [1, width+1, width*2 + 1, 2],
+    [1, width+1, width*2 + 1, width*2],
+    [width, width+1, width+2, width*2+2],
+    [width, width*2, width*2+1, width*2+2]
+    ]
+
+    const Z_tetris = [
+    [0, width, width+1, width*2+1],
+    [width+1, width+ 2, width*2, width*2+1],
+    [0, width, width+1, width*2+1],
+    [width+1, width+2, width*2, width*2+1]
+    ]
+
+    const T_tetris = [
+    [1, width, width+1, width+2],
+    [1, width+ 1, width+2, width*2+1],
+    [width, width+1, width+2, width*2+1],
+    [1, width, width+1, width*2+1]
+    ]
+
+    const O_tetris = [
+    [0, 1, width, width+1],
+    [0, 1, width, width+1],
+    [0, 1, width, width+1],
+    [0, 1, width, width+1]
+    ]
+
+    const I_tetris = [
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3]
+    ]
 })
