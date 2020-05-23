be26e4979c9aa623f7e662d9937a033e6c148cb5
syedmeesamali <syedmeesamali@yahoo.com>
2020-05-23 04:20:41 +0400

    Tetris game in JS + review of some JS

---
+++ 1_Projects/11_JS_Tetris/tetris.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid');
+    let squares = Array.from(document.querySelectorAll('.grid div'));
+    const width = 10;
+    const scoreDisp = document.querySelector('#score');
+    const startButton = document.querySelector('#start-button');
+    
+    console.log(squares);
+})
