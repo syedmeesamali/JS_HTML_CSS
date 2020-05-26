b0027d5d000a9d16f9b37a772ef3ea8a8a022283
syedmeesamali <syedmeesamali@yahoo.com>
2020-05-27 01:24:55 +0400

    After long and hard struggle of almost 15 days - success to get XMLHTTP work - Some trouble was due to browser i.e. use CTRL + F5 to clear cache as latest JS is mostly not loaded even after updates.

--- 1_Projects/7_FlaskNew/Ajax2/app/static/convert.js
+++ 1_Projects/7_FlaskNew/Ajax2/app/static/convert.js
@@ -1,9 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
+        console.log("Latest file!!!!");        
+        document.querySelector('#form').onsubmit = (event) => {
+        event.preventDefault();
         const currency = document.querySelector('#currency').value;
-        document.querySelector('#myForm').onsubmit = () => {
         console.log("Currency entered is: " + currency);
         const request = new XMLHttpRequest();        
+        request.open('POST', '/convert');
         request.onload = () => {
+            console.log("Response text: " + request.responseText);
             const data = JSON.parse(request.responseText);
             if (data.success) {
                 console.log("Success");
@@ -16,9 +20,8 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }
         const data = new FormData();
-        data.append('currency', currency);
-        request.open('POST', '/convert', true);
-        req.setRequestHeader('content-type', 'application/json');
+        data.append('currency', currency);     
+        request.setRequestHeader('content-type', 'application/json');
         request.send(data);
         return false;
         document.querySelectorAll('#currency').value = '';        
--- 1_Projects/7_FlaskNew/Ajax2/app/views.py
+++ 1_Projects/7_FlaskNew/Ajax2/app/views.py
@@ -1,4 +1,5 @@
 from flask import  Flask, render_template, request, jsonify, make_response
+import requests
 from app import app
 import os
 from datetime import datetime
