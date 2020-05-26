654ad0e6cce4f271992c9886e729e323b37e9bab
syedmeesamali <syedmeesamali@yahoo.com>
2020-05-26 04:52:25 +0400

    Final ajax from the JS video - Will be updated soon.

--- 1_Projects/7_FlaskNew/Ajax2/app/static/ajax.js
+++ 1_Projects/7_FlaskNew/Ajax2/app/static/ajax.js
@@ -1,10 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('#myForm').onsubmit = function(event) {
-        event.preventDefault();
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('#formBtn').disabled = true;
+    document.querySelector('#name').onkeyup = () => {
+        document.querySelector('#formBtn').disabled = false;
+    }
+    
+    document.querySelector('#myForm').onsubmit = () => {
+        const li = document.createElement('li');
         const name = document.querySelector('#name').value;
         const email = document.querySelector('#email').value;
-        var res = document.getElementById('result');
-        res.innerHTML = "Name is: " + name + " and email is: " + email;
+        const result = "Name: " + name + "Email: " + email;
+        li.innerHTML = result;
+        document.querySelector("#items").append(li);
+        document.querySelector('#name').value = '';
+        document.querySelector('#email').value = '';
+        document.querySelector('#formBtn').disabled = true;
+        return false;
     }
 });
     // function do_ajax() {
