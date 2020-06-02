017a33f028f425a0289afb58d3d47e61d3cd7a08
syedmeesamali <syedmeesamali@yahoo.com>
2020-06-02 22:56:08 +0400

    Struggling with the toggle in Vanilla JS

--- 5_JavaScript/2_Game/Fruits/fruits.js
+++ 5_JavaScript/2_Game/Fruits/fruits.js
@@ -1,16 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log("Dom is loaded");
-    var img1 = document.querySelector('#fruit-1');
-    img1.addEventListener('click', () => {
-        console.log("Image 1 clicked");
-    if (img1.src == "URL('./img/apple.png')") {
-        img1.src == "URL('./img/letter-a.png')";
-        console.log("Here now!");
-    } else {
-        img1.src == "URL('./img/apple.png')";
-        console.log("Came here!");
+    console.log("Dom is loaded!");
+    
+    var img = document.querySelector("#fruit-1");
+    img.addEventListener('click', () => {
+    var img1 = document.querySelector("#fruit-1").src;
+    console.log("Image 1 clicked");
+    if (img1) {
+        document.querySelector("#fruit-1").src = 'img/apple.png';
+        console.log("First if");
+        console.log("Src = " + document.querySelector("#fruit-1").src);
+    } else if (!img1) {
+        document.querySelector("#fruit-1").src = 'img/letter-a.png';
+        console.log("Else if");
+        console.log("Src = " + document.querySelector("#fruit-1").src);
     }
         
     }) 
 
-})
+    var reset = document.querySelector('#reset');
+    reset.addEventListener('click', () => {
+        console.log("Reset items");
+        document.querySelector("#fruit-1").src = './img/letter-a.png';
+        document.querySelector("#fruit-1").style.visibility = 'hidden';
+    })
+
+
+});
