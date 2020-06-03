15754db25f0a6cbd1e1db0d28360ac5efdb8d58b
syedmeesamali <syedmeesamali@yahoo.com>
2020-06-03 23:47:58 +0400

    Image src control issues continue - unable to toggle

--- 5_JavaScript/2_Game/Fruits/fruits.js
+++ 5_JavaScript/2_Game/Fruits/fruits.js
@@ -1,24 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log("Dom is loaded!");
     
-    var img1 = new Image();
+    //var img1 = new Image();
     var img2 = new Image();
-    img1 = document.querySelector('#fruit-1');
-    img2 = document.querySelector('#fruit-1');
-    img1.src = './img/apple.png';
-    img2.src = './img/letter-a.png';
+    let img1 = document.querySelector('#fruit-1');
+    img2 = img1;
+    img2.src = img1.src;
 
-    //var img = document.querySelector("#fruit-1");
     img1.addEventListener('click', () => {
-    console.log("First Image source: " + img1.src);
+        console.log("Image 1 original source = " + img1.src);
+        img1.style.display = 'none';
+        console.log("Image 1 src now = " + img1.src);
     if (img1) {
         img1.src =img2.src;
-        console.log("First if");
-        console.log("Src = " + img2.src);
+        img1.style.display = 'hidden';
+        //img2.style.display = 'visible';
+        console.log("First if Src = " + img2.src);
     } else {
         img1.src = img1.src;
-        console.log("Else if");
-        console.log("Src = " + img1.src);
+        console.log("Second if Src = " + img1.src);
         
     }
         
@@ -27,8 +27,8 @@ document.addEventListener('DOMContentLoaded', () => {
     var reset = document.querySelector('#reset');
     reset.addEventListener('click', () => {
         console.log("Reset items");
-        document.querySelector("#fruit-1").src = './img/letter-a.png';
-        console.log("Src = " + document.querySelector("#fruit-1").src);
+        let imgCheck = document.querySelector("#fruit-1");
+        imgCheck.style.display = 'hidden';
     })
 
 
