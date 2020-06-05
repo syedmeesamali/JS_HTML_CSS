920db33aef54954b17366176edfcd2e2b27e238f
syedmeesamali <syedmeesamali@yahoo.com>
2020-06-05 23:54:13 +0400

    excellent - simple technique to just hide images after LOADING BOTH of them - all USER side - nothing server side

--- 5_JavaScript/2_Game/Fruits/fruits.js
+++ 5_JavaScript/2_Game/Fruits/fruits.js
@@ -2,17 +2,16 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log("Dom is loaded!");
     var isSwap = true;
     let img1 = document.querySelector('#fruit-1');
-    img1.addEventListener('click',swap());
-    function swap () {
-        if (isSwap) {
-            document.querySelector('#fruit-1').src = "img/apple.png";
-            isSwap = false;
-            console.log("First if");
-        } else {
-            document.querySelector('#fruit-1').src = "img/letter-a.png";
-            isSwap = true;
-            console.log("Else if");
-        }
-        console.log("At end");
-    }     
-}) 
+    let img2 = document.querySelector('#fruit-5');
+    img2.addEventListener('click', () => {
+        console.log("Image src = " + img1.src);
+        img2.style.visibility = 'hidden';
+        img1.style.visibility = 'visible';
+    });
+    img1.addEventListener('click', () => {
+        console.log("Image src = " + img1.src);
+        img1.style.visibility = 'hidden';
+        img2.style.visibility = 'visible';
+    });
+    
+});
