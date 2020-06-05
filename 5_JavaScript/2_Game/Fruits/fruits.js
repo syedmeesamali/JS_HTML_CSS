5358573f7baed0243f3ec1eb297e714890df5b9b
syedmeesamali <syedmeesamali@yahoo.com>
2020-06-05 22:15:13 +0400

    Nneed to learn proper JS

--- 5_JavaScript/2_Game/Fruits/fruits.js
+++ 5_JavaScript/2_Game/Fruits/fruits.js
@@ -1,35 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log("Dom is loaded!");
-    
-    //var img1 = new Image();
-    var img2 = new Image();
+    var isSwap = true;
     let img1 = document.querySelector('#fruit-1');
-    img2 = img1;
-    img2.src = img1.src;
-
-    img1.addEventListener('click', () => {
-        console.log("Image 1 original source = " + img1.src);
-        img1.style.display = 'none';
-        console.log("Image 1 src now = " + img1.src);
-    if (img1) {
-        img1.src =img2.src;
-        img1.style.display = 'hidden';
-        //img2.style.display = 'visible';
-        console.log("First if Src = " + img2.src);
-    } else {
-        img1.src = img1.src;
-        console.log("Second if Src = " + img1.src);
-        
-    }
-        
-    }) 
-
-    var reset = document.querySelector('#reset');
-    reset.addEventListener('click', () => {
-        console.log("Reset items");
-        let imgCheck = document.querySelector("#fruit-1");
-        imgCheck.style.display = 'hidden';
-    })
-
-
-});
+    img1.addEventListener('click',swap());
+    function swap () {
+        if (isSwap) {
+            document.querySelector('#fruit-1').src = "img/apple.png";
+            isSwap = false;
+            console.log("First if");
+        } else {
+            document.querySelector('#fruit-1').src = "img/letter-a.png";
+            isSwap = true;
+            console.log("Else if");
+        }
+        console.log("At end");
+    }     
+}) 
