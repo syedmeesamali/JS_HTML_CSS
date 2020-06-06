fe0c71f78be4a7123061b02f21d2e0231bf8893a
syedmeesamali <syedmeesamali@yahoo.com>
2020-06-06 23:55:27 +0400

    Various functions and methods in JS

--- 5_JavaScript/3_Tinkering/tinker.js
+++ 5_JavaScript/3_Tinkering/tinker.js
@@ -1,5 +1,18 @@
-const nums = [1,2,3,4];
-const results = nums.map((number) => {
-    return number * 2;
-})
-console.log(results);
+const friends = ['ali', 'mohsin', 'aun'];
+const res = friends.map((friend, i) => ({
+    firstName: friend,
+    lastName: 'shah',
+    id: 'lasdfs${i}sdf'
+}));
+console.log(res);
+
+function car(model) {
+    this.model = '911 Carrera';
+}
+var myCar = new car();
+console.log(myCar.model);
+
+function sum(a=1,b=2,c=3) {
+    return a+b+c;
+}
+console.log(sum(5,4,3));
