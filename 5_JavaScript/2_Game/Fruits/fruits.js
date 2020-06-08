86e9d280cc4779f0380d40c65272e71ad65fa713
syedmeesamali <syedmeesamali@yahoo.com>
2020-06-08 17:42:36 +0400

    Need to learn and re-use in some way
    Signed-off-by: syedmeesamali <syedmeesamali@yahoo.com>

--- 5_JavaScript/2_Game/Fruits/fruits.js
+++ 5_JavaScript/2_Game/Fruits/fruits.js
@@ -1,14 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
-    var isSwap = true;
-    let img1 = document.querySelector('#fruit-1');
-    let img2 = document.querySelector('#fruit-5');
-    img2.addEventListener('click', () => {
-        img2.style.visibility = 'hidden';
-        img1.style.visibility = 'visible';
-    });
-    img1.addEventListener('click', () => {
-        img1.style.visibility = 'hidden';
-        img2.style.visibility = 'visible';
-    });
+    console.log("Document loaded!");
+    let num1 = document.getElementById("nums").value;
+    let val1 = parseDozen(num1);
+    let btn = document.getElementById("reset");
+    btn.addEventListener('click', () => {
+        console.log("Val is: " + num1);
+        console.log("Donuts are: " + val1);
+    })
+    var timer = setInterval("document.getElementById('fruit-2').src = './img/figs.png'", 2000);
+    function parseDozen(inputString) {
+        numDons = parseInt(inputString);
+        if (inputString.indexOf("dozen") != -1) {
+            numDons *= 12;
+        } return numDons;
+    }
+})
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     var isSwap = true;
+//     let img1 = document.querySelector('#fruit-1');
+//     let img2 = document.querySelector('#fruit-5');
+//     img2.addEventListener('click', () => {
+//         img2.style.visibility = 'hidden';
+//         img1.style.visibility = 'visible';
+//     });
+//     img1.addEventListener('click', () => {
+//         img1.style.visibility = 'hidden';
+//         img2.style.visibility = 'visible';
+//     });
     
-});
+// });
