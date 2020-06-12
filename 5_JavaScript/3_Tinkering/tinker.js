a2ee895038f42337fb0ee632a9f2bcf66cd418e2
syedmeesamali <syedmeesamali@yahoo.com>
2020-06-12 23:18:01 +0400

    Some tinkering with shapes and colors

--- 5_JavaScript/3_Tinkering/tinker.js
+++ 5_JavaScript/3_Tinkering/tinker.js
@@ -1,29 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('#reset').addEventListener('click', () => {
-        window.location.reload();
+    const colors = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+    const btn = document.getElementById("reset");
+    let shape1 = document.getElementById("square-1");
+
+    btn.addEventListener('click', function() {
+        console.log("Button clicked");
+        const randNumber = 2;
+        shape1.style.backgroundColor = "red";
     })
 })
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     document.querySelector('#reset').addEventListener('click', () => {
-//     console.log("Inside now!");
-//     const request = new XMLHttpRequest();                
-//     request.onload = () => {
-//         if (request.status === 200) {
-//             var elements = request.response.getElementByTagName("a");
-//             for (x of elements) {
-//                 if (x.href.match(/\.(jpe?g|png|gif)$/)) {
-//                     let img = document.createElement("img");
-//                     img.src = x.href;
-//                     document.body.appendChild(img);
-//                 }
-//             }
-//         } else {
-//             alert("request failed: " + request.status);
-//         }
-//     }
-//     request.open('GET', 'localhost/img', true); //Type, URL, Async boolean
-//     request.responseType = 'blob';
-//     request.send();
-//     })
-// });
