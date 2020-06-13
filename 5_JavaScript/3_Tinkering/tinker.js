02f2b2e18270253796bcaf88c999277630ad3552
syedmeesamali <syedmeesamali@yahoo.com>
2020-06-13 23:29:07 +0400

    JS study continues - small projects to consolidate my JS

--- 5_JavaScript/3_Tinkering/tinker.js
+++ 5_JavaScript/3_Tinkering/tinker.js
@@ -1,20 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     const colors = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
-    const btn = document.getElementById("reset");
     let shape1 = document.getElementById("square-1");
 
-    btn.addEventListener('click', function() {
-        let randNumber = "#";
-        for (var i=0; i<6; i++) {
-            randNumber += colors[Math.floor(Math.random() * 15)];
-        }
-        shape1.style.position = "relative";
-        shape1.style.left = shape1.style.top = "10%";
-        shape1.style.backgroundColor = randNumber;
-    })
+    let count = 0;
+    const btn = document.querySelectorAll(".btn");
+    btn.forEach(function (btn) {
+        btn.addEventListener('click', function(e) {
+            console.log(e.currentTarget);
+        })
+    });
 
-    setInterval(changeBox, 500);
-
+    setInterval(changeBox, 1000);
     function changeBox() {
         let randNumber = "#";
         for (var i=0; i<6; i++) {
