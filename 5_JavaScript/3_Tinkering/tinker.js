04afbab209a95edbe3b4df0a74ffad0e0e5cc5a2
syedmeesamali <syedmeesamali@yahoo.com>
2020-06-14 23:46:47 +0400

    JS + CSS transitions
    Signed-off-by: syedmeesamali <syedmeesamali@yahoo.com>

--- 5_JavaScript/3_Tinkering/tinker.js
+++ 5_JavaScript/3_Tinkering/tinker.js
@@ -1,6 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     const colors = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
     let shape1 = document.getElementById("square-1");
+    
+    const text = document.querySelector(".fancy"); //Animate text
+    const stringText = text.textContent;
+    const splitText = stringText.split("");
+    text.textContent = "";
+    for (let i=0; i<splitText.length; i++) {
+        text.innerHTML += "<span>" + splitText[i] + "</span>";
+    }
 
     let count = 0;
     const btn = document.querySelectorAll(".btn");
@@ -14,6 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
             } else {
                 count = 0;
             }
+            shape1.style.font.size = "20px"
             shape1.textContent = count;
         })
     });
