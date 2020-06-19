e4969b3f1a206f2f34d4748b2b80057cd04656d9
syedmeesamali <syedmeesamali@yahoo.com>
2020-06-19 23:50:22 +0400

    Solar system simple animation working perfect now

--- 5_JavaScript/3_Tinkering/solar.js
+++ 5_JavaScript/3_Tinkering/solar.js
@@ -24,8 +24,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
         //Earth drawing
         var time = new Date();
+        ctx.rotate(((2 * Math.PI) / 60) * time.getSeconds() + ((2 * Math.PI)/ 60000) * time.getMilliseconds());
+        ctx.translate(105, 0);
+        ctx.fillRect(0, -12, 40, 24);
+        ctx.drawImage(earth, -12, -12);
 
+        //Draw the moon
+        ctx.save();
+        ctx.rotate(((2 * Math.PI) / 6) * time.getSeconds() + ((2 * Math.PI)/ 6000) * time.getMilliseconds());
+        ctx.translate(0, 28.5);
+        ctx.drawImage(moon, -3.5, -3.5);
+        ctx.restore();
 
+        ctx.restore();
+
+        ctx.beginPath();
+        ctx.arc(150, 150, 105, 0, Math.PI * 2, false); //Earth orbit
+        ctx.stroke();
 
         ctx.drawImage(sun, 0, 0, 300, 300);
         window.requestAnimationFrame(draw);
