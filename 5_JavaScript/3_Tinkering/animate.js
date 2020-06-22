471f8a59faa88d2137d0c1748e77fb5fec9f215d
syedmeesamali <syedmeesamali@yahoo.com>
2020-06-22 12:57:12 +0400

    Interesting use of POP to make balloons pop based on interval
    Signed-off-by: syedmeesamali <syedmeesamali@yahoo.com>

--- 5_JavaScript/3_Tinkering/animate.js
+++ 5_JavaScript/3_Tinkering/animate.js
@@ -2,10 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const colors = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
     
     const canvas = document.getElementById('canvas');
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    //canvas.width = window.innerWidth;
+    //canvas.height = window.innerHeight;
     var brush = canvas.getContext("2d");
-    
+    innerWidth = 800;
+    innerHeight = 500;
+
     //Function to draw random circles    
     function Circle(x, y, dx, dy, radius) {
         this.x = x;
@@ -16,8 +18,8 @@ document.addEventListener('DOMContentLoaded', () => {
         this.color = rgbaColor();
         this.draw = function () {
             brush.beginPath();
-            brush.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
-            brush.strokeStyle = rgbaColor();
+            brush.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false); //2 * Pi is the angle of full circle
+            brush.strokeStyle = 'blue'; 
             brush.fillStyle = this.color;
             brush.fill();
             brush.stroke();
@@ -48,16 +50,52 @@ document.addEventListener('DOMContentLoaded', () => {
     let counter = 0;
     let circleCount = document.getElementById("circleCount");
     canvas.addEventListener('click', function(event) {
+        //If there are no circles then can't check for the hit inside one
+        if (circleArray.length != 0) {
+            circleArray.forEach(function (arrayItem) {
+                var x1 = arrayItem.x;
+                var y1 = arrayItem.y;
+                if (Math.sqrt((event.pageX - x1)^2 + (event.pageY - y1)^2 ) < arrayItem.radius);
+                {
+                    console.log("Clicked in a circle!");
+                    return false;
+                }
+            });
+        }
         counter++;
         let radius = (Math.random() * 45);
-        console.log("Counter is: " + counter);
+        console.log("Counter is: " + counter);    
         let x = event.pageX;
         let y = event.pageY;
         let dx = (Math.random() - 0.5) * 10;
         let dy = (Math.random() - 0.5) * 10;
         circleArray.push(new Circle(x,y,dx,dy,radius));
-        circleCount.textContent = counter;
+        console.log("circleArray.length = " + circleArray.length);
+        circleCount.textContent = circleArray.length;
     })
+
+    //Mouse position function
+    function getMouse(canvas, event) {
+        var rect = canvas.getBoundingClientRect();
+        return {
+            x: event.pageX - rect.left,
+            y: event.pageY - rect.top
+        };
+    }
+    
+    setInterval(popCircle, 2000);
+    
+    //Remove the circles after some interval
+    function popCircle() {
+        if (circleArray.length != 0) {
+            circleArray.pop(Math.floor(Math.random() * circleArray.length));
+        }
+    }
+
+    //Intersection function
+    function interSect(point, circle) {
+        return Math.sqrt((point.x - circle.x) ** 2 + (point.y - circle.y) ** 2) < circle.radius;
+    }
 
     //Main animate function
     function animate(){
