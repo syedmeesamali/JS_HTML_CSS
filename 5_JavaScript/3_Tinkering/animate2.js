07d101018d31f7595f90a84839c29c965eb4c614
syedmeesamali <syedmeesamali@yahoo.com>
2020-06-22 23:55:46 +0400

    Want to make first simple useful and relevant animation or simulation - excited

--- 5_JavaScript/3_Tinkering/animate2.js
+++ 5_JavaScript/3_Tinkering/animate2.js
@@ -1,12 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const colors = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
     const canvas = document.getElementById('canvas');
     var brush = canvas.getContext("2d");
     innerWidth = 400;
     innerHeight = 400;
     let ht = 10;
     let wid = 200;
-    let x = 10; let y = 10;
-    function makeRect(x,y,wid,ht) {
+    let x = 10; let y = 100;
+    
+
+    function drawBase() {
+        brush.beginPath();       // Start a new path
+        brush.moveTo(10, 10);    // Move the pen to (30, 50)
+        brush.lineTo(210, 10);  // Draw a line to (150, 100)
+        brush.stroke();          // Render the path
+    }
+
+    //Make a rectangle with certain properties
+    function makeRect(x,y,wid,ht, fillStyle) {
         this.x = x;
         this.y = y;
         this.wid = wid;
@@ -14,44 +25,42 @@ document.addEventListener('DOMContentLoaded', () => {
         brush.beginPath();
         brush.rect(x,y,wid,ht);
         brush.strokeStyle = 'blue'; 
-        brush.fillStyle = 'red';
+        brush.fillStyle = fillStyle;
         brush.fill();
     }
     
-
-    //Function to draw random rectangles
-    function Rectangle(x, y, dx, dy, size) {
-        this.x = x;
-        this.y = y;
-        this.dx = dx;
-        this.dy = dy;
-        this.size = size;
-        this.color = 'red';
-        this.draw = function () {
-            brush.beginPath();
-            brush.rect(x,y,size,size);
-            brush.strokeStyle = 'blue'; 
-            brush.fillStyle = this.color;
-            brush.fill();
-            brush.stroke();
+    //Random rgba colors
+    function rgbaColor() {
+        let randNumber = "#";
+        for (var i=0; i<6; i++) {
+            randNumber += colors[Math.floor(Math.random() * 15)];
         }
-        //Reverse direction once hitting the wall
-        this.update = function() {
-            this.x += this.dx;
-            this.y += this.dy;
-            this.draw();
-        }
-    } //End of circle function
-
-    let rectArray = [];
+        return randNumber;
+    }
+    
+    let working;
+    //Some movement based on time not clicks
+    function makeMoves() {
+        ht -= 10;
+        //brush.clearRect(0, 0, innerWidth, innerHeight);
+        fillStyle = 'red';
+        makeRect(x,y,wid,ht, fillStyle);
+        if (ht < 10) {
+            ht = 10;
+            makeRect(x,y,wid,ht, fillStyle);
+        }        
+    }
+    //Main event listener
     canvas.addEventListener('click', function(event) {
-        ht += 10;
-        makeRect(x,y,wid,ht);
-        if (ht > 100) {
-            x += 5; y += 5;
-            makeRect(x,y,wid,ht);
+        drawBase();
+        if (!working) {
+            working = window.setInterval(makeMoves, 500);
+            console.log("Working started!");
+        } else {
+            console.log("Working stopped!");
+            window.clearInterval(working);
+            working = null;
         }
-        
     })
 
     
