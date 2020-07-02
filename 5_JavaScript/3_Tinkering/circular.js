0b9f06160cfd001124515bead0f14d5a4c5d5b7c
syedmeesamali <syedmeesamali@yahoo.com>
2020-07-02 23:16:05 +0400

    New circular animation works - part 1

--- 5_JavaScript/3_Tinkering/circular.js
+++ 5_JavaScript/3_Tinkering/circular.js
@@ -10,8 +10,36 @@ document.addEventListener('DOMContentLoaded', () => {
         y : undefined
     }
     
-    //Main object to be moved
+    //Main object which will be drawn
+    function Particle(x, y, radius, color) {
+        this.x = x;     this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.update = () => {
+            this.draw();
+        }
+        this.draw = () => {
+            brush.beginPath();
+            brush.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+            brush.fillStyle = this.color;
+            brush.fill();
+            brush.closePath();
+        }
+    }
+    
+    
+    //Implementation of particles
+    let particles;
+    function init() {
+        particles = [];
+        for (let i=0; i<1; i++){
+            particles.push(new Particle(canvas.width / 2, canvas.height / 2, 
+                5, 'blue'));
+        }
+        console.log(particles);
+    }
 
+    init();
 
     //Main event listener
     canvas.addEventListener('click', function(event) {
@@ -25,4 +53,7 @@ document.addEventListener('DOMContentLoaded', () => {
         requestAnimationFrame(animate);
         brush.clearRect(0, 0, canvas.width, canvas.height);
     }
+    particles.forEach(particles => {
+        particles.update();
+    })
 }) //End of main
