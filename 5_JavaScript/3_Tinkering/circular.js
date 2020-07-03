6627dfa9fbdabf64ac87aa9604ed54a72c8ad6b8
syedmeesamali <syedmeesamali@yahoo.com>
2020-07-03 06:07:53 +0400

    Circular motion animation done - need some more updates

--- 5_JavaScript/3_Tinkering/circular.js
+++ 5_JavaScript/3_Tinkering/circular.js
@@ -4,6 +4,16 @@ document.addEventListener('DOMContentLoaded', () => {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     
+    //Draw central circle
+    function drawCircle() {
+        brush.beginPath();
+        brush.arc(canvas.width/2, canvas.height/2, 40, 0, Math.PI * 2, false);
+        brush.strokeStyle = 'yellow'; //Border color
+        brush.fillStyle = 'yellow'; //Filling color
+        brush.fill();
+        brush.stroke();
+    }
+    
     //Mouse coordinates object
     const mouse = {x : undefined,  y : undefined}
     //Main object which will be drawn
@@ -11,24 +21,25 @@ document.addEventListener('DOMContentLoaded', () => {
         this.x = x;     this.y = y;
         this.radius = radius;
         this.color = color;
-        this.radians = 0;
-        this.velocity = 0.05;
+        this.radians = Math.random() * Math.PI * 2;
+        this.velocity = 0.02;
         this.update = () => {
             this.radians += this.velocity;
-            this.x = x + Math.cos(this.radians) * 100;
+            this.x = x + Math.cos(this.radians) * (Math.random() * 50 + 50); //Circular movement due to cos angle
+            this.y = y + Math.sin(this.radians) * (Math.random() * 50 + 50); //y should have sin angle
             this.draw();    } //End of update
-        this.draw = () => {
-            brush.beginPath();
-            brush.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-            brush.fillStyle = this.color;
-            brush.fill();
-            brush.closePath();   } //End of draw
-    } //End of particle object
+            this.draw = () => {
+                brush.beginPath();
+                brush.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+                brush.fillStyle = this.color;
+                brush.fill();
+                brush.closePath();   } //End of draw
+        } //End of particle object
         
     //Implementation of particles
     let particles = [];
     function init() {
-        for (let i=0; i<1; i++){
+        for (let i=0; i<50; i++){
             particles.push(new Particle(canvas.width / 2, canvas.height / 2, 
                 5, 'blue'));
         }
@@ -47,8 +58,10 @@ document.addEventListener('DOMContentLoaded', () => {
     function animate() {
         requestAnimationFrame(animate);
         brush.clearRect(0, 0, innerWidth, innerHeight);
+        
         for (let j=0; j<particles.length; j++) {
             particles[j].update();
+            drawCircle();
         }
     } //End of animate
     
