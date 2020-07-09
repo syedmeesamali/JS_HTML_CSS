1b4f6282115b71b857979c0870034f0d80ab05f2
syedmeesamali <syedmeesamali@yahoo.com>
2020-07-09 17:41:35 +0400

    Try with one JS issue and objects
    Signed-off-by: syedmeesamali <syedmeesamali@yahoo.com>

--- 5_JavaScript/2_Game/Letters/letters.js
+++ 5_JavaScript/2_Game/Letters/letters.js
@@ -1,13 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log("Document loaded!");
-
-    let cardContainer = document.querySelectorAll(".flip-card");
-    cardContainer.addEventListener('click', flipCard(cardContainer));
     
-    function flipCard(cardContainer) {
-        console.log("Clicked inside!");
-        console.log(cardContainer);
-        cardContainer.style.transform = "rotatey(180deg)";
-        cardContainer.style.transitionDuration = "1.5s";
+    //Main items to be acquired
+    let cardContainer = document.querySelectorAll(".flip-card");
+    //cardContainer.addEventListener('click', flipCard(cardContainer));
+    let arr1 = [1,3,5,7,9];
+    let arr2 = arr1.map(mem => mem + " is odd");
+    console.log(arr2);
+    //Just some course practice - using same project
+    let Person = {
+        name: "ali",
+        age: 34,
+        chars: ["happy", "free", "moody"],
+        company: {comp_name: "CCL", years: 6.5}
     }
+    console.log("Most he is: " + Person.chars[2]);
+
 })
