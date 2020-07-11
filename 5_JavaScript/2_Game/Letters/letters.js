3e3a5b87e79e694373dc52b5e6603dc70922135b
syedmeesamali <syedmeesamali@yahoo.com>
2020-07-11 23:56:13 +0400

    Back to card game and better way to handle click events and do action

--- 5_JavaScript/2_Game/Letters/letters.js
+++ 5_JavaScript/2_Game/Letters/letters.js
@@ -1,19 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log("Document loaded!");
+    let clickToggle = true;  
     
     //Main items to be acquired
-    let cardContainer = document.querySelectorAll(".flip-card");
-    //cardContainer.addEventListener('click', flipCard(cardContainer));
+    /*
+    let cardContainer = document.querySelector(".flip-card");
+    cardContainer.addEventListener('click', function(e) {
+        console.log("Clicked on the card!");
+        if (!clickToggle) return;
+    }); */
+    function handleClick(e) {
+        
+    }
+    
+    
+    
     let arr1 = [1,3,5,7,9];
     let arr2 = arr1.map(mem => mem + " is odd");
-    console.log(arr2);
-    //Just some course practice - using same project
-    let Person = {
-        name: "ali",
-        age: 34,
-        chars: ["happy", "free", "moody"],
-        company: {comp_name: "CCL", years: 6.5}
-    }
-    console.log("Most he is: " + Person.chars[2]);
-
+    
 })
