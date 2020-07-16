d496456c7dc70633f2883748792655ac50069c04
syedmeesamali <syedmeesamali@yahoo.com>
2020-07-16 20:47:17 +0400

    Re-work of the main cards game to review all concepts

--- 5_JavaScript/2_Game/Mix_Match/MixMatch.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let grid1 = document.querySelector('.card');
-    grid1.addEventListener('click', function(e) {
-        console.log("Clicked inside grid");
-    })   
-})
