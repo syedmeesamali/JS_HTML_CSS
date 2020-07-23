4cdef8c48a66e6d38ba64701cfb5c487100f499d
syedmeesamali <syedmeesamali@yahoo.com>
2020-07-23 22:34:51 +0400

    Some updates to the card game and the OOP in JS

--- 5_JavaScript/2_Game/Mix_Match/mixmatch.js
+++ 5_JavaScript/2_Game/Mix_Match/mixmatch.js
@@ -1,3 +1,19 @@
+class AudioController {
+        constructor() {
+                this.bigMusic = new Audio('Assets/Audio/creepy.mp3');
+                this.flipAudio = new Audio('Assets/Audio/flip.wav');
+                this.matchSound = new Audio('Assets/Audio/match.wav');
+                this.victorySound = new Audio('Assets/Audio/victory.wav');
+                this.gameOver = new Audio('Assets/Audio/gameOver.wav');
+                this.bigMusic.volume = 0.5;
+                this.bigMusic.loop = true;
+        }
+        startMusic() {
+                this.bigMusic.play();           //Play the sound method
+        }
+}
+
+
 //Make sure all assets loaded before we use our JS in the page
 if(document.readyState === 'loading') {
         document.addEventListener('DOMContentLoaded', ready());
@@ -15,6 +31,8 @@ function ready() {
                 overlay.addEventListener('click', () => {
                         console.log("Overlay clicked!");
                         overlay.classList.remove('visible');
+                        let audioController = new AudioController();
+                        audioController.startMusic();
                 })
         })
 
