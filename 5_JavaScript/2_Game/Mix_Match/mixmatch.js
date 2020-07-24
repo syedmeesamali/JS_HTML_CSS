d188908bbb8cc549caf62921e3a277a378c8ad7e
syedmeesamali <syedmeesamali@yahoo.com>
2020-07-24 21:50:33 +0400

    Stuck with addeventlistener for button - maybe related to loading of elements or alike

--- 5_JavaScript/2_Game/Mix_Match/mixmatch.js
+++ 5_JavaScript/2_Game/Mix_Match/mixmatch.js
@@ -1,3 +1,42 @@
+//Main document ready function
+function ready() {
+        console.log("Loaded Now !");
+        var myBtn = document.getElementById("myBtn");
+        myBtn.addEventListener("click", alert("hell world"));
+
+
+        function audioPlayer() {
+                console.log("Button clicked");
+                let audioController = new AudioController();
+                audioController.startMusic();
+        }
+                
+        const overLayTotal = document.getElementsByClassName('card');
+        overLayTotal.forEach((ele, index) => {
+        ele.addEventListener('click', (e) => {
+                console.log(index, ele);
+        })
+})
+
+var overlays = Array.from(overLayTotal);  //Create array from HTML elements
+var cards = Array.from(document.getElementsByClassName('card'));
+console.log(cards[0]);
+
+overlays.forEach(overlay => {
+        overlay.addEventListener('click', () => {
+                console.log("Overlay clicked!");
+                overlay.classList.remove('visible');
+                let audioController = new AudioController();
+                audioController.startMusic();
+        })
+})
+cards.forEach(card => { 
+        card.addEventListener('click', () => {
+                console.log("Clicked!");
+        })
+})
+}
+
 class AudioController {
         constructor() {
                 this.bigMusic = new Audio('Assets/Audio/creepy.mp3');
@@ -14,47 +53,12 @@ class AudioController {
 }
 
 
+
 //Make sure all assets loaded before we use our JS in the page
 if(document.readyState === 'loading') {
+        console.log("Loading ....");
         document.addEventListener('DOMContentLoaded', ready());
 } else {
+        console.log("DOM loaded");
         ready();
-}
-
-//Main document ready function
-function ready() {
-        console.log("Dom loaded now!");
-        var clickBtn = document.getElementById("clickmeBtn");
-        
-        clickBtn.addEventListener('click', updateButton);
-
-        function updateButton() {
-                console.log("Button clicked");
-                let audioController = new AudioController();
-                audioController.startMusic();
-        }
-        const overLayTotal = document.getElementsByClassName('card');
-        overLayTotal.forEach((ele, index) => {
-                ele.addEventListener('click', (e) => {
-                        console.log(index, ele);
-                })
-        })
-        
-        var overlays = Array.from(overLayTotal);  //Create array from HTML elements
-        var cards = Array.from(document.getElementsByClassName('card'));
-        console.log(cards[0]);
-        
-        overlays.forEach(overlay => {
-                overlay.addEventListener('click', () => {
-                        console.log("Overlay clicked!");
-                        overlay.classList.remove('visible');
-                        let audioController = new AudioController();
-                        audioController.startMusic();
-                })
-        })
-        cards.forEach(card => { 
-                card.addEventListener('click', () => {
-                        console.log("Clicked!");
-                })
-        })
 }
