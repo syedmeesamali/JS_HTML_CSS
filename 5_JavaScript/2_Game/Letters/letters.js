b5ed57e20cdfddecdb6bbcb130663ea99bf2e0d9
syedmeesamali <syedmeesamali@yahoo.com>
2020-07-28 05:23:27 +0400

    SOme JS for the fruits game as well as the assets update

--- 5_JavaScript/2_Game/Letters/letters.js
+++ 5_JavaScript/2_Game/Letters/letters.js
@@ -1,6 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log("Document loaded!");
-    
-    
-
+    doStuff();
+    //window.onload = doStuff;
+    function doStuff() {
+        console.log("Window content loaded");
+        let cards = Array.from(document.getElementsByClassName('card'));
+        cards.forEach(card => {
+                card.addEventListener('click', () => {
+                    console.log("Card clicked"); 
+                    card.classList.toggle("card-back");
+                })
+        })
+    } //End of do stuff function
 })
