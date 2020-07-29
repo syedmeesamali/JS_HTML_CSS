6e621f78457a57d96b362cf3ffb8bfa4a6cb37ce
syedmeesamali <syedmeesamali@yahoo.com>
2020-07-29 04:43:47 +0400

    Hurrah . ........ just a small change and it all works like a charm

--- 5_JavaScript/3_Tinkering/Letters/letter.js
+++ 5_JavaScript/3_Tinkering/Letters/letter.js
@@ -1,4 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log("Data loaded");
-    
+    let cards = Array.from(document.getElementsByClassName('card'));
+    console.log(cards[0]);
+    cards.forEach(card => {
+        card.addEventListener('click', () => {
+            console.log("Card clicked");
+            if (!card.classList.contains('card-back')) {
+                card.classList.remove('card-front');
+                card.classList.add('card-back');
+            } else {
+                card.classList.add('card-front');
+                card.classList.remove('card-back');
+            }
+        })
+    });
 })
