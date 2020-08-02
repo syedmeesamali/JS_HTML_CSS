20be0af1211b391bad69154490d782626e0bcedc
syedmeesamali <syedmeesamali@yahoo.com>
2020-08-02 11:22:16 +0400

    Opacity did the trick  and solved the whole visible or hidden issue

--- 5_JavaScript/2_Game/Letters/letters.js
+++ 5_JavaScript/2_Game/Letters/letters.js
@@ -1,19 +1,32 @@
+class AudioController {
+    constructor() {
+            this.flipAudio = new Audio('assets/flip.wav');
+    }
+    flip() {   this.flipAudio.play();      }
+ }
+
 document.addEventListener('DOMContentLoaded', () => {
     doStuff();
     function doStuff() {
         let cards = Array.from(document.getElementsByClassName('card-face'));
+        cardFirst = false
         cards.forEach(card => {
+            let audioController = new AudioController();
                 card.addEventListener('click', () => {
                     console.log("Card class:" + card.classList);
-                    if (card.classList.contains('visible')) {
-                        card.classList.remove('visible');
-                        card.classList.add('hidden');
+                    if(!cardFirst) {
+                        card.classList.add("opClass");
+                        audioController.flip();
                         console.log("Card class:" + card.classList);
+                        cardFirst = true;
                     } else {
-                        card.classList.add("card-back","card-face", "visible");
+                        card.classList.remove("opClass");
+                        audioController.flip();
                         console.log("Card class:" + card.classList);
-                    } 
+                        cardFirst = false;
+                    }
                  })
-        })
+        }) //End of foreach
+    
     } //End of do stuff function
 })
