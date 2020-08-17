d1d5ae9143332cb7aea900c495b54f4dee0d7e48
syedmeesamali <syedmeesamali@yahoo.com>
2020-08-17 23:51:14 +0400

    Small issues and updates

--- 1_Projects/12_Quiz-App/script.js
+++ 1_Projects/12_Quiz-App/script.js
@@ -1,14 +1,15 @@
 const startButton = document.getElementById('start-btn');
 const qContainer = document.getElementById('question-container');
 const qElement = document.getElementById('question');
+const answer = document.getElementById('answer-buttons');
 
 
-const shuffleQuestions, currentIndex;
-
+let shuffleQuestions, currentIndex;
 
 
 startButton.addEventListener('click', startGame);
 
+//Start point of the game
 function startGame() {
     startButton.classList.add('hide');
     qContainer.classList.remove('hide');
@@ -18,11 +19,13 @@ function startGame() {
 }
 
 function nextQuestion() {
-
+    showQuestion();
 }
 
+//Show next question
 function showQuestion() {
-
+    qElement.innerText = questions.question;
+    console.log("Question answer: " + questions.answer);
 }
 
 function selectAnswer() {
@@ -31,9 +34,9 @@ function selectAnswer() {
 
 const questions = [
     {
-        question: "What is 2 + 2? ",
+        question: 'What is 2 + 2? ',
         answer: [
-            {  text: '4', correct: true }
+            {  text: '4', correct: true }, 
             {  text: '7', correct: false }
         ]
     }
