24f65ac5e7e289a1f3cb0332a8ffe2a58d9f7915
syedmeesamali <syedmeesamali@yahoo.com>
2020-08-27 22:54:42 +0400

    JS part started for the todo-list - Final from my side should be connection to sqlite

--- 1_Projects/12_Better_ToDo/script.js
+++ 1_Projects/12_Better_ToDo/script.js
@@ -1,1 +1,19 @@
-let lists = []  //Empty to-do list in the start
+const listContainer = document.querySelector('[data-lists]');
+
+let lists = ['name', 'to-do'];  //Empty to-do list in the start
+
+function render() {
+    //<li class="list-name">Work</li>
+    clearElement(listContainer);
+    lists.forEach(list => {
+        const listElement = document.createElement('li');
+        listElement.classList.add('list-name');
+        listElement.innerText = list;
+        listContainer.appendChild(listElement);
+    })
+}
+
+function clearElement(element) {
+}
+
+render()
