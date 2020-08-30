adfe0843f0824b9b42e8518126162bc3408b1972
syedmeesamali <syedmeesamali@yahoo.com>
2020-08-30 23:53:13 +0400

    Local storage related updates

--- 1_Projects/12_Better_ToDo/script.js
+++ 1_Projects/12_Better_ToDo/script.js
@@ -3,9 +3,18 @@ const newListForm = document.querySelector('[data-new-list-form]');
 const newListInput = document.querySelector('[data-new-list-input]'); 
 
 //Using local storage to store the lists locally else keep the lists as empty
-const LOCAL_STORAGE_LIST_KEY = 'task.lists'
-let lists = JSON.parse[localStorage.getItem(LOCAL_STORAGE_LIST_KEY)] || []; 
+const LOCAL_STORAGE_LIST_KEY = 'task.lists';
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId';
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
 
+
+listContainer.addEventListener('click', e => {
+    if (e.target.tagName.toLowerCase() === 'li') {
+        selectedListId = e.target.dataset.listId
+        saveAndRender();
+    }
+})
 
 //Main submit form event listener
 newListForm.addEventListener('submit', e => {
@@ -27,18 +36,24 @@ function saveAndRender() {
     render()
 }
 
+//Save the lists to the local-storage for later retrieval
 function save() { 
     localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
+    localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId);
 }
 
 //Main render function for the overall program
 function render() {
-    clearElement(listContainer);
+    clearElement(listContainer)
     lists.forEach(list => {
         const listElement = document.createElement('li');
         listElement.dataset.listId = list.id;
         listElement.classList.add('list-name');
-        listElement.innerText = list;
+        listElement.innerText = list.name;
+        if (list.id === selectedListId) {
+            listElement.classList.add('active-list')
+        }
+
         listContainer.appendChild(listElement);
     })
 }
