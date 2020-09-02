2a599deddf005d648f168f19b0a49e9e3b43d451
syedmeesamali <syedmeesamali@yahoo.com>
2020-09-02 18:40:07 +0400

    Some checks for CSS
    Signed-off-by: syedmeesamali <syedmeesamali@yahoo.com>

---
+++ 5_JavaScript/4_Misc/checks.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+        doStuff();
+        function doStuff() {
+                console.log("Window content loaded");
+        }// End of doStuff function
+}) //End of main DOM loaded function
