cc0a89a3ddcc3c81e47f665ffaa626723280f543
syedmeesamali <syedmeesamali@yahoo.com>
2020-09-07 23:59:23 +0400

    Some errors

--- 1_Projects/16_Face-Tracking/face.js
+++ 1_Projects/16_Face-Tracking/face.js
@@ -1,156 +1,20 @@
-const listContainer = document.querySelector('[data-lists]');
-const newListForm = document.querySelector('[data-new-list-form]');
-const newListInput = document.querySelector('[data-new-list-input]'); 
-const delListButton = document.querySelector('[data-delete-list-button]');
-const listDisplayContainer = document.querySelector('[data-list-display-container]');
-const listTitleElement = document.querySelector('[data-list-title]');
-const listCountElement = document.querySelector('[data-list-count]');
-const tasksContainer = document.querySelector('[data-tasks]');
-const taskTemplate = document.getElementById('task-template');
-const newTaskForm = document.querySelector('[data-new-task-form]');
-const newTaskInput = document.querySelector('[data-new-task-input]');
-const clearCompleteTask = document.querySelector('[clear-complete-task-button]');
+const video = document.getElementById("video");
 
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models')
+]).then(startVideo)
 
-//Using local storage to store the lists locally else keep the lists as empty
-const LOCAL_STORAGE_LIST_KEY = 'task.lists';
-const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId';
-let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
-let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
+function startVideo() {
+    navigator.getUserMedia(
+    {  video: {}  }, 
+    stream => video.srcObject = stream, 
+    err => console.error(err)
+    )
+}
 
-
-listContainer.addEventListener('click', e => {
-    if (e.target.tagName.toLowerCase() === 'li') {
-        selectedListId = e.target.dataset.listId
-        saveAndRender();
-    }
+video.addEventListener('play', () => {
+    console.log("Working now");
 })
-
-tasksContainer.addEventListener('click', e => {
-    if(e.target.tagName.toLowerCase() === 'input') {
-        const selectedList = lists.find(list => list.id === selectedListId);
-        const selectedTask = selectedList.tasks.find(task => task.id === e.target.id);
-        selectedTask.complete = e.target.checked;
-        save();
-        renderTaskCount(selectedList);
-    }
-})
-
-delListButton.addEventListener('click', e => {
-    lists = lists.filter(list => list.id !== selectedListId);
-    selectedListId = null;
-    saveAndRender();
-})
-
-//Main submit form event listener
-newListForm.addEventListener('submit', e => {
-    e.preventDefault();
-    const listName = newListInput.value;
-    if (listName == null || listName == '') return;
-    const list = createList(listName);
-    newListInput.value = null;
-    lists.push(list);
-    saveAndRender();
-})
-
-//Create new task
-newTaskForm.addEventListener('submit', e => {
-    e.preventDefault();
-    const taskName = newTaskInput.value;
-    if (taskName == null || taskName == '') return;
-    const task = createTask(taskName);
-    newTaskInput.value = null;
-    const selectedList = lists.find(list => list.id === selectedListId);
-    selectedList.tasks.push(task);
-    saveAndRender();
-})
-
-clearCompleteTask.addEventListener('click', e => {
-    const selectedList = lists.find(list => list.id === selectedListId);
-    selectedList.tasks = selectedList.tasks.filter(task => !task.complete);
-    saveAndRender();
-}) 
-
-//Create task function
-function createTask(name) {
-    return { id: Date.now().toString(), name: name, complete: false };
-}
-
-//Create new task list with provided name
-function createList(name) {
-    return { id: Date.now().toString(), name: name, tasks: [] };
-}
-
-//Save the list to local-storage and render it
-function saveAndRender() {
-    save()
-    render()
-}
-
-//Save the lists to the local-storage for later retrieval
-function save() { 
-    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
-    localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId);
-}
-
-//Main render function for the overall program
-function render() {
-    clearElement(listContainer);
-    renderLists();
-    const selectedList = lists.find(list => list.id === selectedListId)
-    if (selectedListId == null) {
-        listDisplayContainer.style.display = 'none';
-    } else { 
-        listDisplayContainer.style.display = '';
-        listTitleElement.innerText = selectedList.name;
-        renderTaskCount(selectedList);
-        clearElement(tasksContainer);
-        renderTasks(selectedList);
-    }
-}
-
-//Render count of balance tasks left
-function renderTaskCount(selectedList) {
-    const incompleteTasksCount = selectedList.tasks.filter(task => 
-        !task.complete).length;
-    const taskString = incompleteTasksCount === 1 ? "task": "tasks";
-    listCountElement.innerText = `${incompleteTasksCount} ${taskString} remaining!`;
-
-}
-
-//Function to render the tasks
-function renderTasks(selectedList) {
-    selectedList.tasks.forEach(task => {
-        const taskElement = document.importNode(taskTemplate.content, true);
-        const checkbox = taskElement.querySelector('input');
-        checkbox.id = task.id;
-        checkbox.checked = task.complete;
-        const label = taskElement.querySelector('label');
-        label.htmlFor = task.id;
-        label.append(task.name);
-        tasksContainer.appendChild(taskElement);
-    })
-}
-
-//Render all the lists
-function renderLists() { 
-    lists.forEach(list => {
-        const listElement = document.createElement('li');
-        listElement.dataset.listId = list.id;
-        listElement.classList.add('list-name');
-        listElement.innerText = list.name;
-        if (list.id === selectedListId) {
-            listElement.classList.add('active-list')
-        }
-        listContainer.appendChild(listElement);
-    })
-}
-
-//Clear any selected element
-function clearElement(element) {
-    while(element.firstChild) {     //Check for first child and if any then remove
-        element.removeChild(element.firstChild);
-    }
-}
-
-render() //Just render the whole damn thing
