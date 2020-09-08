b057c9694a2e6906f2c00cd4eb0b4b023e0f3e64
syedmeesamali <syedmeesamali@yahoo.com>
2020-09-08 23:46:23 +0400

    Face tracking program working fine

--- 1_Projects/16_Face-Tracking/face.js
+++ 1_Projects/16_Face-Tracking/face.js
@@ -17,9 +17,12 @@ function startVideo() {
 
 //Main event listener while the page is loaded
 video.addEventListener('play', () => {
+    const canvas = faceapi.createCanvasFromMedia(video);
+    document.body.append(canvas);
+    const displaySize = { width: video.width, height: video.height };
     setInterval(async () => {
         const detections = await faceapi.detectAllFaces(video, 
-            new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions;
+            new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
             console.log(detections);
         }, 100)
     
