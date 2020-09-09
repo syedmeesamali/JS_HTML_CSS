ee4fef18b37006a995d2d97c11e1c4a5ebebeebb
syedmeesamali <syedmeesamali@yahoo.com>
2020-09-09 05:46:36 +0400

    Excellent and completed the face detection app - Great

--- 1_Projects/16_Face-Tracking/face.js
+++ 1_Projects/16_Face-Tracking/face.js
@@ -20,10 +20,16 @@ video.addEventListener('play', () => {
     const canvas = faceapi.createCanvasFromMedia(video);
     document.body.append(canvas);
     const displaySize = { width: video.width, height: video.height };
+    faceapi.matchDimensions(canvas, displaySize);
     setInterval(async () => {
         const detections = await faceapi.detectAllFaces(video, 
             new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
-            console.log(detections);
-        }, 100)
-    
+            const resizedDetections = faceapi.resizeResults(detections, displaySize);
+            canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+            faceapi.draw.drawDetections(canvas, resizedDetections);
+            faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+            faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+        }, 
+        
+        100) //Repeat every 100ms
 })
