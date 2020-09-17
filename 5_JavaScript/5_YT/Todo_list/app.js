1387d5ef3fbbc8db3b7b213a5d9433c13a38b908
syedmeesamali <syedmeesamali@yahoo.com>
2020-09-17 23:24:22 +0400

    Few updates and also main JS work for app

--- 5_JavaScript/5_YT/Todo_list/app.js
+++ 5_JavaScript/5_YT/Todo_list/app.js
@@ -5,6 +5,16 @@ const todoList = document.querySelector('.todo-list');
 
 
 //Event Listeners
-
+todoButton.addEventListener('click', addToDo);
 
 //Functions
+function addToDo(event) { 
+    event.preventDefault();         //Prevent form from submission
+    console.log("Hello there");
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    const newToDo = document.createElement('li');
+    newToDo.innerText = "Hey";
+    newToDo.classList.add('todo-item');
+    todoDiv.appendChild(newToDo);
+}
