bfc404e5b834b73b04e8ff5c81c9bdbfb10e27fc
syedmeesamali <syedmeesamali@yahoo.com>
2020-09-30 23:40:57 +0400

    Simple drawings review

--- 5_JavaScript/5_Game-2/game.js
+++ 5_JavaScript/5_Game-2/game.js
@@ -1,11 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
-        doStuff();
+        
         const canvas = document.getElementById('canvas');
-        const brush = canvas.getContext('2d');
-        brush.rect(50, 50, 100, 100);
-        brush.fillStyle = '#ffffff';
-        brush.fill();
-        function doStuff() {
-              
-        }// End of doStuff function
+        const ctx = canvas.getContext('2d');
+        const player = {x: 50, y: 50}
+        draw();
+
+        function draw() {
+                let output = `Pos x: ${player.x} y: ${player.y}`;
+                ctx.fillStyle = '#ff3525';
+                ctx.fillRect(player.x, player.y, 100, 100);
+                ctx.font = "24px serif";
+                //ctx.textAlign = 'center';
+                ctx.fillStyle = 'red';
+                ctx.fillText(output, 100, 30);
+
+                //Draw a triangle
+                ctx.beginPath();
+                ctx.fillStyle = 'blue';
+                ctx.moveTo(100, 200);
+                ctx.lineTo(150, 250);
+                ctx.lineTo(150, 150);
+                ctx.fill();
+
+                ctx.beginPath();
+                ctx.arc(150, 75, 50, 0, 2 * Math.PI);
+                ctx.strokeStyle = 'yellow';
+                ctx.fill();
+                ctx.stroke();
+                
+        }
+        
+        
+        
+        
 }) //End of main DOM loaded function
