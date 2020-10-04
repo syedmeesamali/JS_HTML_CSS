72c4bc284cd0e1e1a57551eb9130f9c29a5b7b76
syedmeesamali <syedmeesamali@yahoo.com>
2020-10-04 23:30:34 +0400

    Great updates to game and quite smooth now with requestAnimationFrames

--- 5_JavaScript/5_Game-2/game.js
+++ 5_JavaScript/5_Game-2/game.js
@@ -6,21 +6,21 @@ document.addEventListener('DOMContentLoaded', () => {
         const player2 = {x: 550, y: 50, speed: 5, width: 15, height: 100};   //Player - 2 as object
         const keyz1 = {ArrowRight: false, ArrowLeft: false, ArrowUp: false, ArrowDown: false};
         const keyz2 = {KeyD: false, KeyA: false, KeyW: false, KeyS: false};
-        draw();
+        requestAnimationFrame(draw);
 
         document.addEventListener('keydown', keyDownEvent);
         document.addEventListener('keyup', keyUpEvent);
         
         function keyDownEvent(event) {
-                keyz1[event.code] = true;
-                keyz2[event.code] = true;
-                move();
+                if (event.code in keyz1) { keyz1[event.code] = true; }
+                if (event.code in keyz2) { keyz2[event.code] = true; }
+                //move();
         }
 
         function keyUpEvent(event) {
-                keyz1[event.code] = false;
-                keyz2[event.code] = false;
-                move();
+                if (event.code in keyz1) { keyz1[event.code] = false; }
+                if (event.code in keyz2) { keyz2[event.code] = false; }
+                //move();
         }
 
         function move(){
@@ -32,22 +32,22 @@ document.addEventListener('DOMContentLoaded', () => {
                         else if (keyz2.KeyA) {player2.x -= player2.speed;};
                 if(keyz2.KeyW) {player2.y -= player2.speed;} 
                         else if (keyz2.KeyS) {player2.y += player2.speed;};
-                draw();
+               // draw();
         }
 
         function draw() {
                 ctx.clearRect(0, 0, canvas.width, canvas.height);
+                move();
                 let output = `x1: ${player1.x} y1: ${player1.y} ||| x2: ${player2.x} y2: ${player2.y}`;
                 ctx.fillStyle = 'blue';
                 ctx.fillRect(player1.x, player1.y, player1.width, player1.height);
                 ctx.fillStyle = 'red';
                 ctx.fillRect(player2.x, player2.y, player2.width, player2.height);
-
                 ctx.font = "18px serif";
                 ctx.textAlign = 'left';
                 ctx.fillStyle = 'red';
                 ctx.fillText(output, 100, 30);
-                //drawCircle();
+                requestAnimationFrame(draw);
         }        
     
     //Draw central circle
