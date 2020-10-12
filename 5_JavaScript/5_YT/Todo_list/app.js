55deb7a3ac1ef3290ceb2abe6105d243170d22e8
syedmeesamali <syedmeesamali@yahoo.com>
2020-10-12 23:30:08 +0400

    Fonts loading just fine after so much effort for them to load properly

--- 5_JavaScript/5_YT/Todo_list/app.js
+++ 5_JavaScript/5_YT/Todo_list/app.js
@@ -19,12 +19,12 @@ function addToDo(event) {
     todoDiv.appendChild(newToDo);
     //Add the new buttons
     const completedButton = document.createElement("button");
-    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.innerHTML = '<i class="fa fa-check" aria-hidden="true"></i>';
     completedButton.classList.add("complete-btn");
     todoDiv.appendChild(completedButton);
 
     const trashButton = document.createElement("button");
-    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.innerHTML = '<i class="fa fa-trash" aria-hidden="true"></i>';
     trashButton.classList.add("complete-btn");
     todoDiv.appendChild(trashButton);
 
