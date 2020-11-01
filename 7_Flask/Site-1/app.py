1f231b18db7f38c03aed543deedb84488f609aa9
syedmeesamali <syedmeesamali@yahoo.com>
2020-11-01 17:42:34 +0400

    Some proper reviwe of the flask again - from book
    Signed-off-by: syedmeesamali <syedmeesamali@yahoo.com>

--- 7_Flask/Site-1/app.py
+++ 7_Flask/Site-1/app.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template, make_response
+from flask import , request, Flask, render_template, make_response
+app = Flask(__name__)       #Define the flask app thing
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is {} </p>'.format(user_agent)
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name = name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
