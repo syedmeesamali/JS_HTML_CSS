3f21bca552c8804f5d4e304ba9d1f2b773c4db50
syedmeesamali <syedmeesamali@yahoo.com>
2020-11-07 23:40:40 +0400

    Custom error pages

--- 7_Flask/Site-1/app.py
+++ 7_Flask/Site-1/app.py
@@ -3,10 +3,17 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__)       #Define the flask app thing
 bootstrap = Bootstrap(app)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("404.html"), 500
+
 @app.route('/')
 def index():
-    fruits = ["apples", "berries", "grapes", "oranges"]
-    return render_template('index.html', fruits = fruits)
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
