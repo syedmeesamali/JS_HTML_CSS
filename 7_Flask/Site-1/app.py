5da8f9b73386cc178cafb0d1425d09222b267af0
syedmeesamali <syedmeesamali@yahoo.com>
2020-11-07 23:58:03 +0400

    URL Shortener small app

--- 7_Flask/Site-1/app.py
+++ 7_Flask/Site-1/app.py
@@ -15,9 +15,9 @@ def page_not_found(e):
 def index():
     return render_template('index.html')
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name = name)
+@app.route('/about')
+def about():
+    return '<h2>This is a URL shortener</h2>'
 
 if __name__ == '__main__':
     app.run(debug=True)
