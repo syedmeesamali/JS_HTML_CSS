80a50f7400bd2bdfd540bb8d2eb782ef15e771bc
syedmeesamali <syedmeesamali@yahoo.com>
2020-12-12 05:16:29 +0400

    Excellent perfectly working now for the COMPLETED status of tasks - used a boolean method

--- 7_Flask/Site-2/app.py
+++ 7_Flask/Site-2/app.py
@@ -9,19 +9,12 @@ bootstrap = Bootstrap(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
-done_db = "./done.db"
 
-#Main class for addition, update and deletion of tasks
-class Completed(done_db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    content = db.Column(db.String(200), nullable = False)
-    completed = db.Column(db.Integer, default = 0)
-    date_created = db.Column(db.DateTime, default = datetime.utcnow)
-
+#Class to define the model for TODO list 
 class ToDo(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     content = db.Column(db.String(200), nullable = False)
-    completed = db.Column(db.Integer, default = 0)
+    completed = db.Column(db.Boolean, default = False, nullable = False)
     date_created = db.Column(db.DateTime, default = datetime.utcnow)
 
     def __repr__(self):
@@ -35,6 +28,7 @@ def page_not_found(e):
 def page_not_found(e):
     return render_template("404.html"), 500
 
+#Main display page
 @app.route('/', methods = ['POST', 'GET'])
 def index():
     if request.method == 'POST':
@@ -47,21 +41,22 @@ def index():
         except:
             return "There was some error"
     else:
-        tasks = ToDo.query.order_by(ToDo.date_created).all()
+        tasks = ToDo.query.filter_by(completed = False).all()
         return render_template('index.html', tasks = tasks)
 
+#Completed tasks to be shown - Display method
 @app.route('/Completed', methods = ['POST', 'GET'])
 def completed():
     if request.method == 'POST':
-        tasks = ToDo.query.order_by(ToDo.date_created).all()
+        tasks = ToDo.query.filter_by(completed = True).all()
     else:
-        tasks = ToDo.query.order_by(ToDo.date_created).all()
+        tasks = ToDo.query.filter_by(completed = True).all()
         return render_template('completed.html', tasks = tasks)
 
+#DELETE a task
 @app.route('/delete/<int:id>')
 def delete(id):
     task_to_delete = ToDo.query.get_or_404(id)
-
     try:
         db.session.delete(task_to_delete)
         db.session.commit()
@@ -69,7 +64,7 @@ def delete(id):
     except:
         return "There was some problem deleting that task!"
 
-
+#UPDATE a task
 @app.route('/update/<int:id>', methods = ['POST', 'GET'])
 def update(id):
     task_to_update = ToDo.query.get_or_404(id)
@@ -83,16 +78,16 @@ def update(id):
     else:
         return render_template('update.html', task = task_to_update)
 
+#Mark a task as DONE
 @app.route('/done/<int:id>')
 def done(id):
-    task_done = ToDo.query.get_or_404(id)
-
+    task_done = ToDo.query.get_or_404(id)           #Retrieve the task to be updated
+    task_done.completed = True                      #Set the status to completed
     try:
-        db.session.delete(task_to_delete)
         db.session.commit()
         return redirect('/')
     except:
-        return "There was some problem deleting that task!"
+        return render_template("404.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
