4eacab719c96a6420fe54ef4a11cf710b727227d
syedmeesamali <syedmeesamali@yahoo.com>
2021-01-02 22:23:31 +0400

    Detailed addition of the ORM for DB connection

--- 7_Flask/Site-4 (ecommerce)/app.py
+++ 7_Flask/Site-4 (ecommerce)/app.py
@@ -12,6 +12,16 @@ app.config['SECRET_KEY'] = 'my_rand_secret_key_here'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(20), unique = True, nullable = False)
+    email = db.Column(db.String(120), unique = True, nullable = False)
+    image_file = db.Column(db.String(20), nullable = False, default = 'default.jpg')
+    password = db.Column(db.String(60), nullable = False)
+    
+    def __repr__(self):
+        
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
