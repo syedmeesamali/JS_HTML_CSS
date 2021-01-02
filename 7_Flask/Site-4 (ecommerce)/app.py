20eed363f60c9816ba4286055cbcfd3512ad5df2
syedmeesamali <syedmeesamali@yahoo.com>
2021-01-02 23:16:33 +0400

    ORM model deployed

--- 7_Flask/Site-4 (ecommerce)/app.py
+++ 7_Flask/Site-4 (ecommerce)/app.py
@@ -18,9 +18,20 @@ class User(db.Model):
     email = db.Column(db.String(120), unique = True, nullable = False)
     image_file = db.Column(db.String(20), nullable = False, default = 'default.jpg')
     password = db.Column(db.String(60), nullable = False)
+    posts = db.relationship('Post', backref = 'author', lazy = True)
     
     def __repr__(self):
-        
+        return f"User('{self.username}', '{self.email}', '{self.image}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100), nullable = False)
+    date_posted = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    content = db.Column(db.Text, nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
 
 @app.errorhandler(404)
 def page_not_found(e):
