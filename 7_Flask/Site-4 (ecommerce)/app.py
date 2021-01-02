c241f64bc960a29fbd7272429a38dd0fa4b158e4
syedmeesamali <syedmeesamali@yahoo.com>
2021-01-02 19:52:00 +0400

    Login validation and flash messages added now

--- 7_Flask/Site-4 (ecommerce)/app.py
+++ 7_Flask/Site-4 (ecommerce)/app.py
@@ -44,6 +44,12 @@ def Register():
 @app.route('/Login', methods = ['POST', 'GET'])
 def Login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash(f"You have successfully logged in!", 'success')
+            return redirect(url_for('index'))
+        else:
+            flash(f"Login unsuccessful! Please check username and password", 'danger')
     return render_template('login.html',  title='Login', form = form)
 
 if __name__ == '__main__':
