dd52820f2c426f3ed600e9510f7f9c35f120b310
syedmeesamali <syedmeesamali@yahoo.com>
2021-01-08 23:51:31 +0400

    Validation related updates

--- 7_Flask/Site-4 (ecommerce)/app/forms.py
+++ 7_Flask/Site-4 (ecommerce)/app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, StringField, PasswordField, validators, SubmitField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
+from app.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators = [DataRequired(), Length(min = 4, max = 20)])
@@ -8,6 +9,12 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators = [DataRequired()])
     confirm = PasswordField('Confirm Password', validators = [DataRequired(), EqualTo('password')])
     submit =  SubmitField('Sign Up')
+    
+    def validate_username(self, username):
+        user = User.query.filter_by(username = username.data)
+        if True:
+            raise ValidationError('Validation Message')
+
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators = [DataRequired(), Email()])
