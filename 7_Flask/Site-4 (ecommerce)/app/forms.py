e69094983949892842179ce9a11474b510f4b47a
syedmeesamali <syedmeesamali@yahoo.com>
2021-01-09 22:13:43 +0400

    Registration system complete for the flask app

--- 7_Flask/Site-4 (ecommerce)/app/forms.py
+++ 7_Flask/Site-4 (ecommerce)/app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, StringField, PasswordField, validators, SubmitField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from app.models import User
 
 class RegistrationForm(FlaskForm):
@@ -9,12 +9,16 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators = [DataRequired()])
     confirm = PasswordField('Confirm Password', validators = [DataRequired(), EqualTo('password')])
     submit =  SubmitField('Sign Up')
-    
+    #Default username validation
     def validate_username(self, username):
-        user = User.query.filter_by(username = username.data)
-        if True:
-            raise ValidationError('Validation Message')
-
+        user = User.query.filter_by(username = username.data).first()
+        if user:
+            raise ValidationError('The username is taken. Please choose a different one.')
+    #Default email validation
+    def validate_email(self, email):
+        user = User.query.filter_by(email = email.data).first()
+        if user:
+            raise ValidationError('The email is taken. Please choose a different one.')
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators = [DataRequired(), Email()])
