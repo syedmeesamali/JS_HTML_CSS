5ac6c020dd2920a2a77cccfb78be96781f6c0bb8
syedmeesamali <syedmeesamali@yahoo.com>
2021-01-11 23:48:07 +0400

    Small updates to personal account form

--- 7_Flask/Site-4 (ecommerce)/app/routes.py
+++ 7_Flask/Site-4 (ecommerce)/app/routes.py
@@ -63,5 +63,5 @@ def Logout():
 @login_required
 def Account():
     form = UpdateAccountForm()
-    image_file = url_for('static', filename = 'profile_pics/' + current_user.image_file)
+    image_file = url_for('static', filename = 'img/' + current_user.image_file)
     return render_template('account.html',  title='Account', image_file = image_file, form = form)
