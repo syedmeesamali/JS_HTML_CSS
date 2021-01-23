1b94dfc8c5e09c745a37aa34577b3ca9ea2a0204
syedmeesamali <syedmeesamali@yahoo.com>
2021-01-23 17:12:16 +0400

    Image upload working perfectly and nicely - Need to update further stuff

--- 7_Flask/Site-4 (ecommerce)/app/routes.py
+++ 7_Flask/Site-4 (ecommerce)/app/routes.py
@@ -65,9 +65,11 @@ def Logout():
 #Special save picture function
 def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
-    _, f_ext = os.path.splitext(form_picture)
+    _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    form_picture.save(picture_path)
+    return picture_fn
 
 @app.route('/Account', methods = ['POST', 'GET'])
 @login_required
@@ -75,7 +77,8 @@ def Account():
     form = UpdateAccountForm()
     if form.validate_on_submit():
         if form.picture.data:
-
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
         current_user.username = form.username.data
         current_user.email = form.email.data
         db.session.commit()
