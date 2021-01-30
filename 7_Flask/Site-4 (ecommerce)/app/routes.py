bd13d89e739f00b7f5f51510a607b1b462014241
syedmeesamali <syedmeesamali@yahoo.com>
2021-01-30 21:03:55 +0400

    Jump to individual posts uploaded by any user

--- 7_Flask/Site-4 (ecommerce)/app/routes.py
+++ 7_Flask/Site-4 (ecommerce)/app/routes.py
@@ -106,3 +106,9 @@ def new_post():
         flash(f'Your post has been created successfully', 'success')
         return redirect(url_for('index'))
     return render_template('create_new.html', title = 'New post', form = form)
+
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = Post.query.get_or_404(post_id)    
+    return render_template('post.html', title = post.title, post = post)
