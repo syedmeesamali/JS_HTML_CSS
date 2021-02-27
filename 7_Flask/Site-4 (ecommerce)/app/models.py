ae2b5f2667da0b1b6785480327b42662aadabad5
syedmeesamali <syedmeesamali@yahoo.com>
2021-02-27 23:51:42 +0400

    Review of expiry tokens

--- 7_Flask/Site-4 (ecommerce)/app/models.py
+++ 7_Flask/Site-4 (ecommerce)/app/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-from app import db, login_manager
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from app import db, login_manager, app
 from flask_login import UserMixin
 
 @login_manager.user_loader
@@ -14,6 +15,10 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(60), nullable = False)
     posts = db.relationship('Post', backref = 'author', lazy = True)
     
+    def get_reset_token(self, expires_seconds = 1800):
+        s = Serializer(app.config['SECRET_KEY'], expires_seconds)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image}')"
 
