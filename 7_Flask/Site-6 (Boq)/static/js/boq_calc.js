66d08f549fa7d635c4abdd379f1451f8b460fda2
syedmeesamali <syedmeesamali@yahoo.com>
2021-02-28 18:00:52 +0400

    Some updates to BOQ calculation
    Signed-off-by: syedmeesamali <syedmeesamali@yahoo.com>

--- 7_Flask/Site-6 (Boq)/static/js/boq_calc.js
+++ 7_Flask/Site-6 (Boq)/static/js/boq_calc.js
@@ -33,8 +33,33 @@ document.addEventListener('DOMContentLoaded', () => {
         result_5 = result_5.toFixed(2);
         document.querySelector("#result5").innerHTML = formatNumber(result_5);
 
-        var final_result = Number(result_1) + Number(result_2) + + Number(result_3) + 
-                           Number(result_4) + Number(result_5);
+        var qty_val6 = document.getElementById("qty_val6").value;
+        var price_val6 = document.getElementById("price_val6").value;
+        var result_6 = qty_val6 * price_val6;
+        result_6 = result_6.toFixed(2);
+        document.querySelector("#result6").innerHTML = formatNumber(result_6);
+
+        var qty_val7 = document.getElementById("qty_val7").value;
+        var price_val7 = document.getElementById("price_val7").value;
+        var result_7 = qty_val7 * price_val7;
+        result_7 = result_7.toFixed(2);
+        document.querySelector("#result7").innerHTML = formatNumber(result_7);
+
+        var qty_val8 = document.getElementById("qty_val8").value;
+        var price_val8 = document.getElementById("price_val8").value;
+        var result_8 = qty_val8 * price_val8;
+        result_8 = result_8.toFixed(2);
+        document.querySelector("#result8").innerHTML = formatNumber(result_8);
+
+        var qty_val9 = document.getElementById("qty_val9").value;
+        var price_val9 = document.getElementById("price_val9").value;
+        var result_9 = qty_val9 * price_val9;
+        result_9 = result_9.toFixed(2);
+        document.querySelector("#result9").innerHTML = formatNumber(result_9);
+
+        var final_result = Number(result_1) + Number(result_2) + Number(result_3) + 
+                           Number(result_4) + Number(result_5) + Number(result_6) + 
+                           Number(result_7) + Number(result_8) + Number(result_9);
         final_result = final_result.toFixed(2);
         document.querySelector("#final_result").innerHTML = formatNumber(final_result);
         
