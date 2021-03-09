17e9efb57b8f56902cf5d1c66851f425b590f753
syedmeesamali <syedmeesamali@yahoo.com>
2021-03-09 23:54:20 +0400

    Updates and further work on the TF image recognition program

--- 7_Flask/Site-7 (TF_Image)/static/images.js
+++ 7_Flask/Site-7 (TF_Image)/static/images.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const fileInput =  document.getElementById("file-input");
   const image = document.getElementById("image");
+  let model;      //To later work with the mobilenet model
 
   //Get the image from the input box / button
   function getImage(){
@@ -10,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
     
     //File reader is an asynchronous function.
     const reader = new FileReader();
-    
+    console.log("Image is here!");
     reader.onload = function(event) {
       image.setAttribute("src", event.target.result);
       document.body.classList.add("image-loaded");
@@ -19,6 +20,16 @@ document.addEventListener('DOMContentLoaded', () => {
     reader.readAsDataURL(file);
   } //End of getImage
 
-  fileInput.addEventListener("change", getImage);
+
+  //Async loading of the main model
+  mobilenet.load().then(function (m) {
+    //save the model
+    model = m;
+    document.body.classList.remove("loading");
+    
+    //When user loads image, should be displayed
+    fileInput.addEventListener("change", getImage);
+
+  })
 
 });
