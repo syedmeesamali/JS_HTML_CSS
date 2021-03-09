2f85c04d4f8fcf257d2746c37c0da96fa604d038
syedmeesamali <syedmeesamali@yahoo.com>
2021-03-09 12:13:11 +0400

    File input and display working good - TF JS for online recognition
    Signed-off-by: syedmeesamali <syedmeesamali@yahoo.com>

--- 7_Flask/Site-7 (TF_Image)/static/images.js
+++ 7_Flask/Site-7 (TF_Image)/static/images.js
@@ -3,6 +3,22 @@ document.addEventListener('DOMContentLoaded', () => {
   const fileInput =  document.getElementById("file-input");
   const image = document.getElementById("image");
 
-  fileInput.addEventListener("change", getImageDataUrl);
+  //Get the image from the input box / button
+  function getImage(){
+    if (!fileInput.files[0]) throw new Error("Image not found");
+    const file = fileInput.files[0];
+    
+    //File reader is an asynchronous function.
+    const reader = new FileReader();
+    
+    reader.onload = function(event) {
+      image.setAttribute("src", event.target.result);
+      document.body.classList.add("image-loaded");
+    }
+
+    reader.readAsDataURL(file);
+  } //End of getImage
+
+  fileInput.addEventListener("change", getImage);
 
 });
