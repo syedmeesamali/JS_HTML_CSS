01231db3ffbb002ff73e79f6297fe0f68a270868
syedmeesamali <syedmeesamali@yahoo.com>
2021-03-14 15:32:34 +0400

    Some updates to the image recognition using tensorflow
    Signed-off-by: syedmeesamali <syedmeesamali@yahoo.com>

--- 7_Flask/Site-7 (TF_Image)/static/images.js
+++ 7_Flask/Site-7 (TF_Image)/static/images.js
@@ -4,32 +4,29 @@ document.addEventListener('DOMContentLoaded', () => {
   const image = document.getElementById("image");
   let model;      //To later work with the mobilenet model
 
-  //Get the image from the input box / button
-  function getImage(){
+function getImage(){
     if (!fileInput.files[0]) throw new Error("Image not found");
     const file = fileInput.files[0];
     
     //File reader is an asynchronous function.
     const reader = new FileReader();
-    console.log("Image is here!");
+    //console.log("Image is here!");
     reader.onload = function(event) {
-      image.setAttribute("src", event.target.result);
-      document.body.classList.add("image-loaded");
-    }
+      const dataUrl = event.target.result;
+      const imageElement = new Image();
+      imageElement.src = dataUrl;
+      imageElement.onload = function () {
+        image.setAttribute("src", this.src);
+        image.setAttribute("height", this.height);
+        image.setAttribute("width", this.width);
 
+        //Classify
+        classifyImage();
+        }
+        document.body.classList.add("image-loaded");
+      }
     reader.readAsDataURL(file);
-  } //End of getImage
-
-
-  //Async loading of the main model
-  mobilenet.load().then(function (m) {
-    //save the model
-    model = m;
-    document.body.classList.remove("loading");
+    } //End of getImage
+  
     
-    //When user loads image, should be displayed
-    fileInput.addEventListener("change", getImage);
-
-  })
-
 });
