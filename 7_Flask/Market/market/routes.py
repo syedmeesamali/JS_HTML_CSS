6bbda8a6d3881b3a856fde191bfdf410cf87b36f
syedmeesamali <syedmeesamali@yahoo.com>
2021-04-02 22:20:06 +0400

    Updates on the registration related forms and data

--- 7_Flask/Market/market/routes.py
+++ 7_Flask/Market/market/routes.py
@@ -1,7 +1,8 @@
 from market import app
-from flask import render_template
-from market.models import Item
+from flask import render_template, redirect, url_for
+from market.models import Item, User
 from market.forms import RegisterForm
+from market import db
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -24,7 +25,13 @@ def market_page():
     return render_template('market.html', items = items)
 
 #Registration
-@app.route('/register')
+@app.route('/register', methods = ['GET', 'POST'])
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username = form.username.data, email = form.email.data, 
+                            password_hash = form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('market_page'))
     return render_template('register.html', form = form)
