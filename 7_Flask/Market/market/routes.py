d306bed10d13fa26d283eb5130321bc9ec3d24ed
syedmeesamali <syedmeesamali@yahoo.com>
2021-04-05 02:04:15 +0400

    Some review and updates

--- 7_Flask/Market/market/routes.py
+++ 7_Flask/Market/market/routes.py
@@ -33,4 +33,7 @@ def register_page():
         db.session.add(user_to_create)
         db.session.commit()
         return redirect(url_for('market_page'))
+    if form.errors != {}:
+        for err_msg in form.errors.values():
+            print(f"There was an error: {err_msg}")
     return render_template('register.html', form = form)
