61a8a77b29c62dd71a2d418ee19bf402fcf0ff31
syedmeesamali <syedmeesamali@yahoo.com>
2021-04-11 22:45:25 +0500

    Bcrypt for hashing of passwords

--- 7_Flask/Market/market/models.py
+++ 7_Flask/Market/market/models.py
@@ -1,12 +1,22 @@
 from market import db
+from market import bcrypt
 
 class User(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(length=30), nullable=False, unique=True)
     email = db.Column(db.String(length=50), nullable=False, unique=True)
     password = db.Column(db.String(length=60), nullable=False)
-    budget = db.Column(db.Integer(), nullable=False, default = 1000)
+    budget = db.Column(db.Integer(), default = 1000)
     items = db.relationship('Item', backref = 'owned_user', lazy=True)
+
+    @property
+    def password(self):
+        return self.password
+
+    @password.setter
+    def password(self, plain_password):
+        self.password_hash = bcrypt.generate_password_hash(plain_password).decode('utf-8')
+
 
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
