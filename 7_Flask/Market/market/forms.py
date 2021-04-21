98706e87e25aafdf8f0fa7cb81baf36db2a0d728
syedmeesamali <syedmeesamali@yahoo.com>
2021-04-21 23:56:29 +0400

    Loginform handler

--- 7_Flask/Market/market/forms.py
+++ 7_Flask/Market/market/forms.py
@@ -4,7 +4,6 @@ from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationE
 from market.models import User
 
 class RegisterForm(FlaskForm):
-    
     #Validation of username if it already exists or not
     def validate_username(self, username_check):
         user = User.query.filter_by(username = username_check.data).first()
@@ -21,4 +20,9 @@ class RegisterForm(FlaskForm):
     email = StringField(label='Email: ', validators=[Email(), DataRequired()])
     password1 = PasswordField(label='Password: ', validators=[Length(min=3, max=20), DataRequired()])
     password2 = PasswordField(label='Confirm Password: ', validators=[EqualTo('password1')])
+    submit = SubmitField(label='Create Account')
+
+class LoginForm(FlaskForm):
+    username = StringField(label='User Name: ', validators = [DataRequired()])
+    password = PasswordField(label='Password: ', validators = [DataRequired()])
     submit = SubmitField(label='Create Account')
