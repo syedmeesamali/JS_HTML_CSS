0a3458c50aa31c946027af917e38bd3ba716f18b
syedmeesamali <syedmeesamali@yahoo.com>
2021-04-25 23:57:32 +0400

    Login manager with password and hashing

--- 7_Flask/Market/market/models.py
+++ 7_Flask/Market/market/models.py
@@ -1,7 +1,14 @@
-from market import db
+from market import db, login_manager
 from market import bcrypt
+from flask_login import UserMixin
 
-class User(db.Model):
+#This is important to take care of the user sessions
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+#Main user model
+class User(db.Model, UserMixin):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(length=30), nullable=False, unique=True)
@@ -10,7 +17,14 @@ class User(db.Model):
     budget = db.Column(db.Integer, nullable=False, default = 1000)
     items = db.relationship('Item', backref = 'owned_user', lazy=True)
 
+    @property
+    def password(self):
+        return self.password
 
+    def check_password(self, attempted_password):
+        return bcrypt.check_password_hash(self.password_hash, attempted_password)
+
+#Main item model
 class Item(db.Model):
     __tablename__ = 'item'
     id = db.Column(db.Integer, primary_key=True)
--- 7_Flask/Market/market/routes.py
+++ 7_Flask/Market/market/routes.py
@@ -3,6 +3,7 @@ from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
 from market.forms import RegisterForm, LoginForm
 from market import db, bcrypt
+from flask_login import login_user
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -46,4 +47,12 @@ def register_page():
 @app.route('/login', methods = ['GET', 'POST'])
 def login_page():
     form = LoginForm()
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+        if attempted_user and attempted_user.check_password(attempted_password = form.password.data):
+            login_user(attempted_user)
+            flash(f'Successfully logged in as: {attempted_user.username}', category='success')
+            return redirect(url_for('market_page'))
+        else:
+            flash(f'Username and/or password does not match', category='danger')
     return render_template('login.html', form=form)
